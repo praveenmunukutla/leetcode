f6dc72830ec35215c8bbe9454d008d53ede9d0ef
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 21:55:52 +0530

    Time: 40 ms (99.74%) | Memory: 96.4 MB (5.96%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+     int speed = []() { 
+        ios::sync_with_stdio(false); // Disable synchronization with C I/O 
+        cin.tie(NULL); // Untie cin from cout 
+        cout.tie(NULL); // Untie cout (though this is not necessary) 
+        return 0; 
+    }(); 
+
     int maxProfit(vector<int>& prices) {
         int buyPrice = prices[0];
         int profit = 0;
