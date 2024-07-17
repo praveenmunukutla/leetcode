d15090423acebb4d0f4d34a00b585f83a4df2169
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 07:08:39 +0530

    Time: 4 ms (84.49%) | Memory: 12.9 MB (27.21%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,56 +1,23 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        unordered_map<char, int> roman = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        
         int sum = 0;
         int len = s.length();
-        for(int i = 0; i < len;i++){
-            if(i < len-1 && s[i] == 'I' && s[i+1] == 'V'){
-               sum += 4;
-               i++;
-            }
-            else if(i < len-1 && s[i] == 'I' && s[i+1] == 'X'){
-               sum += 9;
-               i++;
-            }
-            else if(i < len-1 && s[i] == 'X' && s[i+1] == 'L'){
-               sum += 40;
-               i++;
-            }
-            else if(i < len-1 && s[i] == 'X' && s[i+1] == 'C'){
-               sum += 90;
-               i++;
-            }
-            else if(i < len-1 && s[i] == 'C' && s[i+1] == 'D'){
-               sum += 400;
-               i++;
-            }
-            else if(i < len-1 && s[i] == 'C' && s[i+1] == 'M'){
-               sum += 900;
-               i++;
-            }
-            else if(s[i] == 'I'){
-                sum += 1;
-            }
-            else if(s[i] == 'V'){
-                sum += 5;
-            }
-            else if(s[i] == 'X'){
-                sum += 10;
-            }
-            else if(s[i] == 'L'){
-                sum += 50;
-            }
-            else if(s[i] == 'C'){
-                sum += 100;
-            }
-            else if(s[i] == 'D'){
-                sum += 500;
-            }
-            else if(s[i] == 'M'){
-                sum += 1000;
+        
+        for (int i = 0; i < len; ++i) {
+            // If the current value is less than the next value, subtract it
+            if (i < len - 1 && roman[s[i]] < roman[s[i + 1]]) {
+                sum -= roman[s[i]];
+            } else {
+                sum += roman[s[i]];
             }
         }
-
+        
         return sum;
     }
 };
