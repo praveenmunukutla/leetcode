cd2b2909952ba17f40ffefe3826b5446b989dbfb
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 06:07:12 +0530

    Time: 4 ms (84.18%) | Memory: 8.5 MB (30.58%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -8,11 +8,6 @@ public:
         while(x){
             int d = x%10;
             
-            if(reverse > INT_MAX/10) return false;
-
-            if(reverse == INT_MAX/10){
-                if(d > INT_MAX%10) return false;
-            }
 
             reverse = reverse*10+d;
             x = x/10;
