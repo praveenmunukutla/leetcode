9ce07a4d161f04722d8df205adde42a6ca4c7311
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 20:21:59 +0530

    Time: 9 ms (44.99%) | Memory: 20.1 MB (5.48%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -12,20 +12,27 @@
 class Solution {
 public:
     int speed = []() {
-        ios::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
+        ios::sync_with_stdio(false); // Disable synchronization with C I/O
+        cin.tie(NULL); // Untie cin from cout
+        cout.tie(NULL); // Untie cout (though this is not necessary)
         return 0;
     }();
 
+    /**
+     * Function to check if there exists a path from the root to a leaf node
+     * where the sum of node values equals the given target sum.
+     * @param root The root node of the binary tree.
+     * @param targetSum The target sum to be checked.
+     * @return True if such a path exists, otherwise false.
+     */
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root == nullptr) return false;
+        if (root == nullptr) return false; // If the root is null, return false
 
-        if(root->left == nullptr && root->right == nullptr && targetSum == root->val) return true;
+        // If it's a leaf node and the targetSum matches the node's value, return true
+        if (root->left == nullptr && root->right == nullptr && targetSum == root->val) return true;
 
-        if(root->left == nullptr && root->right == nullptr && targetSum != root->val) return false;
-
-        return hasPathSum(root->left, targetSum-root->val) ||
-        hasPathSum(root->right, targetSum-root->val);
+        // Recursively check the left and right subtrees for the remaining target sum
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
     }
 };
