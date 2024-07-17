a51e5dc3ffc1d80e4141541e2201e071238fb61d
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 16:29:29 +0530

    Time: 5 ms (25.98%) | Memory: 10.6 MB (6.50%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -4,19 +4,25 @@ public:
         int len = digits.size();
 
         // Add 1 to the last digit and handle carry
-        digits[len - 1] = digits[len - 1] + 1;
-        int carry = digits[len - 1] == 10 ? 1 : 0;
+        digits[len - 1] += 1;
+        int carry = digits[len - 1] / 10;
         digits[len - 1] %= 10;
 
-        if(carry == 0) return digits;
+        // Check if there's no carry after incrementing the last digit
+        if (carry == 0) {
+            return digits;
+        }
 
         // Traverse from the second last digit to the first
-        for (int i = len - 2; i >= 0; i--) {
-            digits[i] = digits[i] + carry;
-            carry = digits[i] == 10 ? 1 : 0;
+        for (int i = len - 2; i >= 0; --i) {
+            digits[i] += carry;
+            carry = digits[i] / 10;
             digits[i] %= 10;
 
-            if(carry == 0) return digits;
+            // Return early if no further carry
+            if (carry == 0) {
+                return digits;
+            }
         }
 
         // If there's still a carry left after all digits are processed, insert 1 at the beginning
