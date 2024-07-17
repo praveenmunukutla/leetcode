d9e00245059e5c42971d7972942ca983a21897bb
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 09:39:37 +0530

    Time: 15 ms (29.44%) | Memory: 12.9 MB (14.92%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
     unordered_map<char,int> map = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
 public:
     int romanToInt(string s) {
-        //roman to Int follow rules.
+        // roman to Int follow rules.
         // only certain subtractive combinations are allowed.
         int sum = 0, len = s.length();
         for(int i = 0; i < len; i++){
