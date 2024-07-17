5ee1c9ffa496ea568c09ffdc3a62a2f6b17cbe7f
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 18:09:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (6.68%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int left = 2, right = x, mid = 0;
-        while(left <= right){
-            mid = left + ((right - left)/2);
+        // Handle the edge case where x is 0 or 1
+        if (x < 2) {
+            return x;
+        }
 
-            if(mid == x / mid){
+        int left = 1, right = x / 2, mid = 0;
+        while (left <= right) {
+            mid = left + ((right - left) / 2);
+            
+            // To avoid overflow, compare mid with x / mid instead of mid * mid with x
+            if (mid == x / mid) {
                 return mid;
-            }
-            else if(mid < x / mid){
+            } else if (mid < x / mid) {
                 left = mid + 1;
-            }
-            else {
+            } else {
                 right = mid - 1;
             }
         }
 
+        // At this point, right is the integer part of the square root
         return right;
     }
 };
