98bb46bf1db95b1fcb57dfdbed63e0847a291e95
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 22:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.70%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,9 +8,16 @@
  */
 class Solution {
 public:
+     int speed = []() {
+        ios::sync_with_stdio(false); // Disable synchronization with C I/O 
+        cin.tie(NULL); // Untie cin from cout 
+        cout.tie(NULL); // Untie cout (though this is not necessary) 
+        return 0; 
+    }(); 
+
     bool hasCycle(ListNode *head) {
         if(head == nullptr) return false;
-        
+
         ListNode* rabbit = head->next;
         ListNode* tortoise = head;
 
