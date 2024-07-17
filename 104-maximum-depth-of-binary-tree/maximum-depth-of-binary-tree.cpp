806e4dacd320125fb22012bbd849e15d8fbea888
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 19:53:25 +0530

    Time: 8 ms (38.32%) | Memory: 17.8 MB (5.43%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -10,35 +10,9 @@
  * };
  */
 class Solution {
-    int depth; // Variable to store the maximum depth
 public:
-    Solution() : depth(0) {}
-
-    /**
-     * Function to find the maximum depth of a binary tree.
-     * @param root The root node of the binary tree.
-     * @return The maximum depth of the tree.
-     */
     int maxDepth(TreeNode* root) {
-        if(root == nullptr) return 0; // If the tree is empty, return 0
-        maxDepthUtil(root, 1); // Start the depth calculation from the root with initial depth 1
-        return depth; // Return the maximum depth found
-    }
-
-private:
-    /**
-     * Helper function to recursively find the maximum depth of the tree.
-     * @param root The current node.
-     * @param cdepth The current depth of the node.
-     */
-    void maxDepthUtil(TreeNode* root, int cdepth){
-        if(root == nullptr) return; // If the current node is null, return
-
-        // Update the maximum depth if the current depth is greater than the recorded maximum depth
-        depth = cdepth > depth ? cdepth : depth;
-        
-        // Recursively find the depth of the left and right subtrees
-        maxDepthUtil(root->left, cdepth + 1);
-        maxDepthUtil(root->right, cdepth + 1);
+        if(root == nullptr) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
