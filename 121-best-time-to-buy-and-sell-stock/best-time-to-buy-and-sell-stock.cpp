82862a76e8a884be3566c098fb7b019428793a06
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-17 22:01:28 +0530

    Time: 62 ms (98.17%) | Memory: 96.4 MB (5.96%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-     int speed = []() { 
+    int speed = []() { 
         ios::sync_with_stdio(false); // Disable synchronization with C I/O 
         cin.tie(NULL); // Untie cin from cout 
         cout.tie(NULL); // Untie cout (though this is not necessary) 
@@ -8,17 +8,19 @@ public:
     }(); 
 
     int maxProfit(vector<int>& prices) {
-        int buyPrice = prices[0];
-        int profit = 0;
+        int buyPrice = prices[0]; // Initialize buyPrice with the first element of prices
+        int profit = 0; // Initialize profit to 0
 
-        for(int i = 1; i < prices.size(); i++){
-            if(prices[i] < buyPrice){
+        for(int i = 1; i < prices.size(); i++) {
+            if(prices[i] < buyPrice) { // If current price is less than buyPrice, update buyPrice
                 buyPrice = prices[i];
             }
 
-            profit = max(profit, prices[i]-buyPrice);
+            // Calculate the maximum profit that can be achieved by selling at prices[i]
+            // and buying at the lowest price encountered so far (buyPrice)
+            profit = max(profit, prices[i] - buyPrice);
         }
 
-        return profit;
+        return profit; // Return the maximum profit found
     }
 };
