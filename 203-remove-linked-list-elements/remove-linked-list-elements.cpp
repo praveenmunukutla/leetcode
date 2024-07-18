b66c2fe00a5f86315e55d8039ae96718f45010b2
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-18 11:41:05 +0530

    Time: 16 ms (49.83%) | Memory: 20.1 MB (7.03%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -10,6 +10,13 @@
  */
 class Solution {
 public:
+     int speed = []() {
+        ios::sync_with_stdio(false); // Disable synchronization with C I/O 
+        cin.tie(NULL); // Untie cin from cout 
+        cout.tie(NULL); // Untie cout (though this is not necessary) 
+        return 0; 
+    }(); 
+
     ListNode* removeElements(ListNode* head, int val) {
         if(head == nullptr) return nullptr;
 
@@ -21,7 +28,8 @@ public:
         while(head){
             if(head->val == val){
                 prev->next = head->next;
-            }else
+            }
+            else 
                 prev = head;
             head = head->next;
         }
