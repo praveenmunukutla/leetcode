f442b16497cd78a47a84b955a5b273c1f2a4701e
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-18 07:55:41 +0530

    Time: 3 ms (31.78%) | Memory: 8.1 MB (8.18%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+     int speed = []() {
+        ios::sync_with_stdio(false); // Disable synchronization with C I/O 
+        cin.tie(NULL); // Untie cin from cout 
+        cout.tie(NULL); // Untie cout (though this is not necessary) 
+        return 0; 
+    }(); 
+
     int hammingWeight(int n) {
         int ans = 0;
         while(n){
