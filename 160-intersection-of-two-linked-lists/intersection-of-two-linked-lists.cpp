f0f3835380d666956cff6ef08ba9915205cbce4a
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-18 06:20:36 +0530

    Time: 28 ms (97.83%) | Memory: 17.5 MB (21.80%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,42 +8,33 @@
  */
 class Solution {
 public:
-     int speed = []() {
+    int speed = []() {
         ios::sync_with_stdio(false); // Disable synchronization with C I/O 
         cin.tie(NULL); // Untie cin from cout 
         cout.tie(NULL); // Untie cout (though this is not necessary) 
         return 0; 
-    }(); 
+    }();
 
-    int Length(ListNode* head){
-        if(head == nullptr) return 0;
+    int getLength(ListNode* head){
         int len = 0;
-        while(head){
-            len++;
-            head = head->next;
-        }
+        for(; head; head = head->next) len++;
         return len;
     }
-    
+
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if(headA == nullptr || headB == nullptr) return nullptr;
-        int lenA = Length(headA);
-        int lenB = Length(headB);
-        int diff = lenA > lenB ? lenA-lenB : lenB-lenA;
+        
+        int lenA = getLength(headA);
+        int lenB = getLength(headB);
+        
+        // Adjust the starting point so that the remaining lengths of both lists are equal
         if(lenA > lenB){
-            while(diff && headA){
-                headA = headA->next;
-                diff--;
-            }
-        }else {
-            while(diff && headB){
-                headB = headB->next;
-                diff--;
-            }
+            for(int i = 0; i < lenA - lenB; i++) headA = headA->next;
+        } else {
+            for(int i = 0; i < lenB - lenA; i++) headB = headB->next;
         }
-
-        if(diff) return nullptr;
-
+        
+        // Move both pointers until they intersect
         while(headA && headB){
             if(headA == headB) return headA;
             headA = headA->next;
