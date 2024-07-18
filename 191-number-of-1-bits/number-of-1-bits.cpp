d578a87e4173c621fdc6e2feb6039e32595f0daa
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-18 07:58:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (8.18%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-     int speed = []() {
+    // Initialization block to speed up I/O operations
+    int speed = []() {
         ios::sync_with_stdio(false); // Disable synchronization with C I/O 
         cin.tie(NULL); // Untie cin from cout 
         cout.tie(NULL); // Untie cout (though this is not necessary) 
         return 0; 
     }(); 
 
+    // Function to calculate the Hamming weight of an integer n
     int hammingWeight(int n) {
-        int ans = 0;
+        int ans = 0; // Initialize a variable to store the count of 1 bits
+        
+        // Iterate through each bit of n
         while(n){
-            if(n&1)ans++;
-            n = n >> 1;
+            if(n & 1)  // Check if the least significant bit (LSB) of n is 1
+                ans++; // Increment the count if the LSB is 1
+            n = n >> 1; // Shift n right by 1 bit to process the next bit
         }
-        return ans;
+        
+        return ans; // Return the count of 1 bits (Hamming weight)
     }
 };
