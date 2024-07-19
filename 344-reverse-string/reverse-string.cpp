92e552b164426b4f15ae9d01ccebe75da4a177ff
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-19 18:46:44 +0530

    Time: 15 ms (75.31%) | Memory: 27.3 MB (5.81%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int length = s.size();
+    // Function to reverse the characters in the vector s
+    void reverseString(std::vector<char>& s) {
+        int length = s.size();  // Get the size of the vector
 
-        for(int i  = 0; i < length/2; i++){
-            swap(s[i], s[length-i-1]);
+        // Loop through the first half of the vector
+        for (int i = 0; i < length / 2; i++) {
+            // Swap the element at index i with the element at index length - i - 1
+            std::swap(s[i], s[length - i - 1]);
         }        
     }
 };
