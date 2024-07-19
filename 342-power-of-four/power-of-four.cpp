c2a4aa395d36a51a50084ef338d2872bbf8c96db
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-19 18:59:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (10.45%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 0) return false;
-        
-        while(n){
-            if(n == 1) return true;
-            if(n%4 != 0) return false;
+        // Check if n is positive
+        if (n <= 0) return false;
+
+        // Loop to divide n by 4
+        while (n > 1) {
+            // If n is not divisible by 4, it's not a power of four
+            if (n % 4 != 0) return false;
+            // Divide n by 4
             n = n / 4;
         }
 
-        return true;
+        // If n is reduced to 1, it is a power of four
+        return n == 1;
     }
 };
