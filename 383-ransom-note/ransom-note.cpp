5c45997bbe1df1d4624e33d5ca0e33dfbb311b31
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-20 14:15:18 +0530

    Time: 11 ms (81.53%) | Memory: 11.3 MB (38.79%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,6 +1,13 @@
 class Solution {
     unordered_map<char,int> map;
 public:
+      int speed = []() {
+        ios::sync_with_stdio(false); // Disable synchronization with C I/O 
+        cin.tie(NULL); // Untie cin from cout 
+        cout.tie(NULL); // Untie cout (though this is not necessary) 
+        return 0; 
+    }(); 
+
     bool canConstruct(string ransomNote, string magazine) {
         for(auto ch : magazine)map[ch]++;
         for(auto r : ransomNote){
