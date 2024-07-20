306429da6bd5264f49185e981ea6fa88d148d811
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-20 15:18:16 +0530

    Time: 3 ms (27.10%) | Memory: 7.4 MB (10.69%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    string ans;
 public:
-    string toHex(int num) {
-        if(num == 0) return "0";
+    std::string toHex(int num) {
+        if (num == 0) return "0";  // Handle the special case for zero
 
-        unsigned int n = num;
-        string hex = "0123456789abcdef";
+        unsigned int n = static_cast<unsigned int>(num); // Convert to unsigned to handle negative numbers
+        std::string hex = "0123456789abcdef";
+        std::string ans;
 
-        while(n > 0){
-            int d = n%16;
-            ans.push_back(hex[d]);
-            n = n / 16;
+        while (n > 0) {
+            int d = n % 16; // Get the remainder (hex digit)
+            ans.push_back(hex[d]); // Append the corresponding hex character
+            n = n / 16; // Divide by 16 to process the next digit
         }
         
-        reverse(ans.begin(), ans.end());
+        std::reverse(ans.begin(), ans.end()); // Reverse the string as digits are collected in reverse order
         return ans;
     }
 };
