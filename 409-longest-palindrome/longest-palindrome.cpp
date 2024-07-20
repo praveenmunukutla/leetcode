89b5459ad2cba5ea1f3baf4203d1ffc1b163f79c
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-20 17:07:12 +0530

    Time: 6 ms (23.74%) | Memory: 9.3 MB (13.25%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,22 +1,30 @@
 class Solution {
-   unordered_map<char, int> umap;
+   unordered_map<char, int> umap;  // To store the frequency of each character in the string
+
 public:
     int longestPalindrome(string s) {
+        // Count the frequency of each character in the string
         for(auto c : s) umap[c]++;
-        int ans = 0;
+        
+        int ans = 0;  // To store the length of the longest palindrome
+        bool hasOdds = false;  // Flag to check if there is at least one character with an odd frequency
 
-        bool hasOdds = false;
+        // Iterate through each character in the frequency map
         for(auto u : umap){
-            if(u.second % 2 == 0)ans += u.second;
-            else {
-                ans += u.second-1;
-                hasOdds = true;
+            if(u.second % 2 == 0) {
+                // If the frequency is even, add it fully to the palindrome length
+                ans += u.second;
+            } else {
+                // If the frequency is odd, add the largest even part (count - 1) to the palindrome length
+                ans += u.second - 1;
+                hasOdds = true;  // Mark that we have at least one character with an odd frequency
             }
         }
         
+        // If there was at least one odd frequency, we can place one odd character in the center of the palindrome
         if(hasOdds)
-            ans+=1;
+            ans += 1;
         
-        return ans;
+        return ans;  // Return the length of the longest palindrome that can be built
     }
 };
