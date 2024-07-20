03305f17b579241d8402e4ba00bd3ecc2cadfaaa
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-20 19:22:36 +0530

    Time: 7 ms (31.10%) | Memory: 8.5 MB (11.31%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-
-
         int i = 1;
         int count = 0;
         while(i <= n){
