267b0c7c5ae703f08ae7e3bbb7ae0a439ef24200
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-21 12:37:25 +0530

    Time: 27 ms (70.38%) | Memory: 28.5 MB (19.99%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.cpp
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        // Return 0 if there are no attacks
         if (timeSeries.empty()) return 0;
         
-        int totalDuration = 0;
-        int endTime = 0;
+        int totalDuration = 0; // Total duration of poisoning
+        int endTime = 0;       // End time of the current poisoning interval
         
+        // Iterate through each attack time in the timeSeries
         for (int t : timeSeries) {
-            if (t >= endTime) {
-                totalDuration += duration;
-            } else {
-                totalDuration += (t + duration - endTime);
+            totalDuration += duration; // Initially add the full duration
+            
+            // If the current attack overlaps with the previous one
+            if (endTime > t) {
+                totalDuration -= (endTime - t); // Subtract the overlapping part
             }
+            
+            // Update the end time to the end of the current attack's duration
             endTime = t + duration;
         }
         
-        return totalDuration;
+        return totalDuration; // Return the total poisoned duration
     }
 };
