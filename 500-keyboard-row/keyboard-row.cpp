22e50b430ce95aa43d4024f1d25d4e971df84610
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-21 12:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (6.31%) - LeetSync

--- 500-keyboard-row/keyboard-row.cpp
+++ 500-keyboard-row/keyboard-row.cpp
@@ -1,27 +1,33 @@
 class Solution {
-    unordered_map<char,int> key;
-    vector<string> ans;
+    unordered_map<char, int> key; // Map to store the row number for each character
+    vector<string> ans; // Vector to store the final list of words that can be typed using one row
 public:
     vector<string> findWords(vector<string>& words) {
+        // Strings representing each row of the keyboard
         string one = "qwertyuiop";
         string two = "asdfghjkl";
         string three = "zxcvbnm";
 
-        for(auto c : one)key[c]=1;
-        for(auto c : two)key[c]=2;
-        for(auto c : three)key[c]=3;
+        // Assign each character in the rows a corresponding row number in the map
+        for (auto c : one) key[c] = 1;
+        for (auto c : two) key[c] = 2;
+        for (auto c : three) key[c] = 3;
 
-        int k, i;
-        for(auto w : words){
-            k = key[tolower(w[0])];
-            i = 1;
-            for(i = 1; i < w.length(); i++){
-                if(key[tolower(w[i])] != k) break;
+        // Iterate through each word in the input list
+        for (auto w : words) {
+            // Get the row number of the first character in the word
+            int k = key[tolower(w[0])];
+            int i;
+            
+            // Check if all characters in the word belong to the same row
+            for (i = 1; i < w.length(); i++) {
+                if (key[tolower(w[i])] != k) break;
             }
 
-            if(i >= w.length()) ans.push_back(w);
+            // If the word is valid (all characters in the same row), add it to the result list
+            if (i == w.length()) ans.push_back(w);
         }
         
-        return ans;
+        return ans; // Return the list of valid words
     }
 };
