f5b7d7db1b5213d86474f332470625bc9ef04236
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-21 17:20:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (17.76%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    // bool hasAlternatingBits(int n) {
+    //     int fb = n & 1;  // Get the least significant bit (LSB)
+    //     n = n >> 1;      // Shift right to check the next bit
+
+    //     while (n > 0) {
+    //         int sb = n & 1;  // Get the current LSB
+
+    //         // If the current bit is the same as the previous bit, return false
+    //         if (sb == fb) return false;
+
+    //         // Update the previous bit and shift right to check the next bit
+    //         fb = sb;
+    //         n = n >> 1;
+    //     }
+
+    //     return true;  // If all adjacent bits are different, return true
+    // }
     bool hasAlternatingBits(int n) {
-        
-        int fb = n & 1;
-        n = n >> 1;
-
-        while(n > 0){
-            int sb = n & 1;
-
-            if(sb == fb) return false;
-
-            fb = sb;
-
-            n = n >> 1;
-        }
-
-        return true;
+        long long int x = (n ^ (n >> 1));
+        return (x & (x+1)) == 0;
     }
 };
