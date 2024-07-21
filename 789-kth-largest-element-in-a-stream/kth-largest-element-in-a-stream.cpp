6b9158293f1a4bb503cb2180fe23578ef63cc895
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-21 19:35:29 +0530

    Time: 36 ms (6.67%) | Memory: 24.8 MB (14.87%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,25 +1,31 @@
 class KthLargest {
-    std::priority_queue<int, std::vector<int>, std::greater<int>> minheap;
+    priority_queue<int, vector<int>, greater<int>> minheap;
     int k;
 public:
-    KthLargest(int k, std::vector<int>& nums) : k(k) {
-        for(int num : nums) {
-            if(minheap.size() < k) {
-                minheap.push(num);
-            } else if (num > minheap.top()) {
+    KthLargest(int k, vector<int>& nums):k(k) {
+        for(int i = 0; i < nums.size(); i++){
+            if(minheap.size() < k){
+                minheap.push(nums[i]);
+            }else if(nums[i] > minheap.top()){
                 minheap.pop();
-                minheap.push(num);
+                minheap.push(nums[i]);
             }
         }
     }
     
     int add(int val) {
-        if(minheap.size() < k) {
+        if(minheap.size()<k){
             minheap.push(val);
-        } else if(val > minheap.top()) {
+        }else if(val > minheap.top()){
             minheap.pop();
             minheap.push(val);
         }
         return minheap.top();
     }
 };
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
