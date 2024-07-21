9d52c3848c1acc1deb58530b0f586798835dd5af
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-21 15:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (7.84%) - LeetSync

--- 504-base-7/base-7.cpp
+++ 504-base-7/base-7.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     string convertToBase7(int num) {
-        if(num == 0) return "0";
+        if (num == 0) return "0"; // Handle the special case for 0
         
         string ans = "";
-        bool isnegative = num < 0 ? true : false;
-        if(isnegative) num = num*-1;
-        while(num){
-            int d = num%7;
-            ans.insert(0, to_string(d));
-            num = num/7;
+        bool isNegative = num < 0; // Check if the number is negative
+        if (isNegative) num = -num; // Convert num to positive for processing
+        
+        while (num > 0) { // Process the number while it is greater than 0
+            int d = num % 7; // Get the remainder when divided by 7
+            ans.insert(0, to_string(d)); // Insert the digit at the beginning of the result string
+            num /= 7; // Update num by dividing it by 7
         }
-
-        if(isnegative)
-            ans.insert(0,"-");
-
-        return ans;
+        
+        if (isNegative) 
+            ans.insert(0, "-"); // Add the negative sign if the original number was negative
+        
+        return ans; // Return the result
     }
 };
