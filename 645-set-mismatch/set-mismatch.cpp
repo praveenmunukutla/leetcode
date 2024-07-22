8a691243d703f0e05facc93ae0f2607c20055028
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-22 20:39:12 +0530

    Time: 15 ms (92.53%) | Memory: 24.1 MB (63.77%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,16 +1,23 @@
 class Solution {
-    vector<int> ans;
 public:
-    vector<int> findErrorNums(vector<int>& nums) {
+    std::vector<int> findErrorNums(std::vector<int>& nums) {
+        std::vector<int> ans(2);  // Prepare the answer vector with two slots
         
-        for(int i = 0; i < nums.size(); i++){
-            int index = abs(nums[i])-1;
-            if(nums[index] < 0) ans.push_back(index+1);
-            else nums[index] *= -1;
+        // First pass: find the duplicated number
+        for(int i = 0; i < nums.size(); i++) {
+            int index = std::abs(nums[i]) - 1;  // Find the index corresponding to the current value
+            if(nums[index] < 0) {
+                ans[0] = index + 1;  // If the value at index is negative, we found the duplicate
+            } else {
+                nums[index] = -nums[index];  // Mark the value at index as visited by making it negative
+            }
         }
 
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] > 0) ans.push_back(i+1);   
+        // Second pass: find the missing number
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > 0) {
+                ans[1] = i + 1;  // If the value at index is positive, the corresponding number is missing
+            }
         }
         
         return ans;
