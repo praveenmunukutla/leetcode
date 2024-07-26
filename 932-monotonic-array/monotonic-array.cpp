17bc886a3f0bac55516d7c04821af39601d5e075
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-26 20:13:13 +0530

    Time: 121 ms (27.49%) | Memory: 99.1 MB (24.66%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        int monotonic = 0; //1-increasing; 2-decreasing; 0- didnt decide yet.
-        for(int i = 1; i < nums.size(); i++){
-            if(monotonic == 0) {
-                if(nums[i]==nums[i-1]) continue;
-                else if(nums[i] > nums[i-1]) monotonic = 1;
-                else monotonic = 2;
-            }
-            else {
-                if(monotonic == 1 && nums[i] < nums[i-1]) return false;
-                else if(monotonic == 2 && nums[i] > nums[i-1]) return false;
+        int monotonic = 0; // 1 - increasing, 2 - decreasing, 0 - not decided yet
+
+        // Iterate through the vector to check for monotonicity
+        for(int i = 1; i < nums.size(); i++) {
+            if (monotonic == 0) { // If direction is not yet decided
+                if (nums[i] == nums[i-1]) {
+                    continue; // If current element is equal to previous, continue checking
+                } else if (nums[i] > nums[i-1]) {
+                    monotonic = 1; // Set direction to increasing
+                } else {
+                    monotonic = 2; // Set direction to decreasing
+                }
+            } else { // If direction is already decided
+                if (monotonic == 1 && nums[i] < nums[i-1]) {
+                    return false; // If supposed to be increasing and current element is smaller, return false
+                } else if (monotonic == 2 && nums[i] > nums[i-1]) {
+                    return false; // If supposed to be decreasing and current element is larger, return false
+                }
             }
         }
-        return true;
+        return true; // If no contradictions found, return true
     }
 };
