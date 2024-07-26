4cfa1c13809e9e3e753a10113f515c1e75f7589c
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-26 22:20:01 +0530

    Time: 74 ms (50.00%) | Memory: 86.1 MB (17.15%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
         int five = 0, ten = 0;
-        for(auto b : bills){
-            if(b == 5) five++;
-            else if(b == 10){
-                if(five == 0) return false;
-                five--;
-                ten++;
-            }else if(b == 20){
-                if(ten > 0 && five > 0){
-                    five--, ten--;
-                }else if( five > 2){
-                    five -= 3;
-                }else 
-                    return false;
+
+        for (auto b : bills) {
+            if (b == 5) {
+                five++; // Increase count of $5 bills
+            } else if (b == 10) {
+                if (five == 0) return false; // If no $5 bill is available, return false
+                ten++; // Increase count of $10 bills
+                five--; // Use one $5 bill as change
+            } else { // b == 20
+                if (ten > 0 && five > 0) { 
+                    ten--; // Use one $10 bill
+                    five--; // Use one $5 bill
+                } else if (five >= 3) {
+                    five -= 3; // Use three $5 bills
+                } else {
+                    return false; // Cannot provide correct change
+                }
             }
         }
-        
-        return true;
+
+        return true; // Successfully provided change for all customers
     }
 };
