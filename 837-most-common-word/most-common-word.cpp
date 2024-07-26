449ec32ed22edc1d2d31e829e4cbdace312b75eb
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-26 08:48:42 +0530

    Time: 10 ms (8.16%) | Memory: 12.5 MB (10.41%) - LeetSync

--- 837-most-common-word/most-common-word.cpp
+++ 837-most-common-word/most-common-word.cpp
@@ -1,42 +1,48 @@
 class Solution {
-    unordered_map<string, int> umap;
-    unordered_set<string> uset;
+    unordered_map<string, int> umap; // Frequency map for words
+    unordered_set<string> uset;       // Set of banned words
+    
 public:
-    vector<string> getTokens(string s){
-        stringstream ss(s);
-        vector<string> ans;
+    // Tokenizes the input string by splitting on spaces
+    vector<string> getTokens(string s) {
+        stringstream ss(s); // Create a stringstream from the input string
+        vector<string> ans; // Vector to store tokens
         string token;
-        while(getline(ss, token, ' ')){
-            if(!token.empty()) ans.push_back(token);
+        while (getline(ss, token, ' ')) { // Split by spaces
+            if (!token.empty()) ans.push_back(token); // Add non-empty tokens
         }
         return ans;
     }
 
+    // Finds the most common word in the paragraph that is not banned
     string mostCommonWord(string paragraph, vector<string>& banned) {
-        uset.insert(banned.begin(), banned.end());
-        // Transform the string to lowercase
+        uset.insert(banned.begin(), banned.end()); // Insert banned words into the set
+
+        // Convert paragraph to lowercase and replace non-alphanumeric characters with spaces
         std::transform(paragraph.begin(), paragraph.end(), paragraph.begin(),
-                   [](unsigned char c) { 
-                    if(!isalnum(c)) return (int)' ';
-                    return std::tolower(c); 
-                });
+                       [](unsigned char c) {
+                           return std::isalnum(c) ? std::tolower(c) : ' ';
+                       });
 
-        auto tokens = getTokens(paragraph);
-        for(auto t : tokens){
-            if(!uset.count(t))
+        auto tokens = getTokens(paragraph); // Get tokens from the transformed paragraph
+
+        // Count the frequency of each word that is not banned
+        for (const auto& t : tokens) {
+            if (!uset.count(t)) {
                 umap[t]++;
-        }
-
-        int ans = 0;
-        string answord;
-        for(auto u : umap){
-            cout<<u.first<<" "<<u.second<<endl;
-            if(u.second > ans){
-                answord = u.first;
-                ans = u.second;
             }
         }
 
-        return answord;
+        // Find the word with the highest frequency
+        int maxCount = 0;
+        string mostCommonWord;
+        for (const auto& u : umap) {
+            if (u.second > maxCount) {
+                mostCommonWord = u.first;
+                maxCount = u.second;
+            }
+        }
+
+        return mostCommonWord; // Return the most common word
     }
 };
