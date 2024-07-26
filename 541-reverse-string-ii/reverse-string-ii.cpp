b944574ccbc9b084a322d2e59d697f89dbfbab19
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-26 14:07:04 +0530

    Time: 3 ms (84.72%) | Memory: 9.2 MB (12.14%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -1,29 +1,36 @@
 class Solution {
-    string sc;
-    int slen;
+    string sc;  // Holds the input string for manipulation
+    int slen;   // Stores the length of the input string
+
 public:
     string reverseStr(string s, int k) {
-        sc = s;
-        slen = s.length();
-        bool reversestr = true;
+        sc = s;           // Copy the input string to the class member
+        slen = s.length(); // Determine the length of the input string
+        bool reversestr = true;  // Flag to indicate whether to reverse the substring
+
+        // Iterate over the string in steps of k
         for(int i = 0; i < slen; i = i + k){
             if(reversestr){
-                reverse(i, i+k-1);
+                reverse(i, i + k - 1);  // Reverse the substring if the flag is true
             }
-            reversestr = !reversestr;
+            reversestr = !reversestr;   // Toggle the flag for the next iteration
         }
 
-        return sc;
+        return sc;  // Return the modified string
     }
 
+private:
     void reverse(int i, int j){
-        if(j > slen -1)
-            j = slen-1;
+        // Ensure j does not exceed the string length
+        if(j > slen - 1)
+            j = slen - 1;
+
+        // Reverse the substring from index i to j
         while(i < j){
-            char temp = sc[i];
+            char temp = sc[i];  // Swap characters at indices i and j
             sc[i] = sc[j];
             sc[j] = temp;
-            i++;
+            i++;  // Move indices towards the center
             j--;
         }
     }
