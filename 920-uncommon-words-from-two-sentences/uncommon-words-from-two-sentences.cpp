392feeda7b138a213c54c15f7cc7b85424875959
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-26 22:41:27 +0530

    Time: 5 ms (20.75%) | Memory: 8.8 MB (33.14%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -1,11 +1,12 @@
+
 class Solution {
 public:
-    unordered_map<string,int> getTokens(string s){
+    unordered_map<string,int> getTokens(const string& s) {
         unordered_map<string,int> tokens;
         string token;
         stringstream ss(s);
-        while(getline(ss,token,' ')){
-            if(!token.empty())
+        while (getline(ss, token, ' ')) {
+            if (!token.empty())
                 tokens[token]++;
         }
         return tokens;
@@ -15,11 +16,16 @@ public:
         vector<string> ans;
         auto tokens1 = getTokens(s1);
         auto tokens2 = getTokens(s2);
-        for(auto s1 : tokens1){
-            if(tokens2.find(s1.first) == tokens2.end() && tokens1[s1.first] == 1) ans.push_back(s1.first);
+
+        for (const auto& token1 : tokens1) {
+            if (tokens2.find(token1.first) == tokens2.end() && token1.second == 1) {
+                ans.push_back(token1.first);
+            }
         }
-        for(auto s2 : tokens2){
-            if(tokens1.find(s2.first) == tokens1.end() && tokens2[s2.first] == 1) ans.push_back(s2.first);
+        for (const auto& token2 : tokens2) {
+            if (tokens1.find(token2.first) == tokens1.end() && token2.second == 1) {
+                ans.push_back(token2.first);
+            }
         }
         return ans;
     }
