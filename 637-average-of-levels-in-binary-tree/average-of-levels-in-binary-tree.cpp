254df8dcc5c214810bfae79c706396d2e27bfa1f
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-26 14:34:27 +0530

    Time: 9 ms (75.73%) | Memory: 22.6 MB (23.49%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
@@ -9,29 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> ans;
-        queue<TreeNode*> que;
-        que.push(root);
+        vector<double> ans;          // To store the average of values at each level
+        queue<TreeNode*> que;        // Queue for level order traversal
+        que.push(root);              // Start with the root node
 
+        // Continue while there are nodes to process
         while(!que.empty()){
-            int size = que.size();
-            int csize = size;
-            double sum = 0;
+            int size = que.size();   // Number of nodes at the current level
+            int csize = size;        // Store the size for calculating average
+            double sum = 0;          // Sum of values at the current level
+
+            // Process each node at the current level
             while(size--){
-                auto top = que.front(); que.pop();
+                TreeNode* top = que.front();  // Get the front node in the queue
+                que.pop();                    // Remove the front node from the queue
+
+                // Add child nodes to the queue for the next level
                 if(top->left) que.push(top->left);
                 if(top->right) que.push(top->right);
-                
-                sum += top->val;
-                
+
+                sum += top->val;  // Add the value of the current node to the sum
             }
 
-            ans.push_back(sum/csize);
+            ans.push_back(sum / csize);  // Calculate and store the average for the current level
         }
 
-        return ans;
+        return ans;  // Return the vector containing the averages for each level
     }
 };
