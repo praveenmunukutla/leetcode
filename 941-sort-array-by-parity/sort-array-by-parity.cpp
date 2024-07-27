406cebc74e93ccb412ac1c45928faa25005245bb
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-27 11:44:21 +0530

    Time: 3 ms (94.29%) | Memory: 19.3 MB (14.39%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     std::vector<int> sortArrayByParity(std::vector<int>& nums) {
         // Sort the array using a custom comparator
-        std::sort(nums.begin(), nums.end(), [](const int& a, const int& b) {
+        std::sort(nums.begin(), nums.end(), [](auto a, auto b) {
             // Custom comparator: sorts by parity (even numbers before odd numbers)
             return (a % 2) < (b % 2);
         });
