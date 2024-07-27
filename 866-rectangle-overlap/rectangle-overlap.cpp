e4c5101d6ad3ace7b1f65c8d6177cd5fdd4858f5
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-27 08:15:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (19.40%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.cpp
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -10,3 +10,44 @@ public:
         return x1 < x4 && x3 < x2 && y1 < y4 && y3 < y2;
     }
 };
+
+/*
+ Case-1: Rec2 intersects with Rec1 on top right corner
+
+               ____________________x4,y4
+              |                   |
+       _______|______x2,y2        |
+      |       |______|____________|
+      |      x3,y3   |
+      |______________|
+     x1,y1
+              
+     Case-2: Rec2 intersects with Rec1 on top left corner
+       ___________________  x4,y4
+      |                   |
+      |            _______|____________x2,y2
+      |___________|_______|           |
+    x3,y3         |                   | 
+                  |___________________|
+                x1,y1
+
+     Case-3: Rec2 intersects with Rec1 on bottom left corner
+     
+               ____________________x2,y2
+              |                   |
+       _______|______x4,y4        |
+      |       |______|____________|
+      |      x1,y1   |
+      |______________|
+     x3,y3
+              
+     Case-4: Rec2 intersects with Rec1 on bottom right corner
+     
+       ___________________  x2,y2
+      |                   |
+      |            _______|____________x4,y4
+      |___________|_______|           |
+    x1,y1         |                   | 
+                  |___________________|
+                x3,y3
+*/
