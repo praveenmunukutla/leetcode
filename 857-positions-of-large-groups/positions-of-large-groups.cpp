c882dece3fccca218e8c7ccc6f48e19637e51502
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-27 08:22:34 +0530

    Time: 5 ms (35.86%) | Memory: 9 MB (22.15%) - LeetSync

--- 857-positions-of-large-groups/positions-of-large-groups.cpp
+++ 857-positions-of-large-groups/positions-of-large-groups.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> largeGroupPositions(string s) {
+        // Vector to store the result, which will be a list of pairs of indices
         vector<vector<int>> ans;
 
-        int len = s.length();
-        int i = 0;
-        while(s[i]!='\0'){
-            char ch = s[i];
-            int start = i;
-            int j = i+1, count = 1;
-            while(j < len && s[j] == ch){
+        int len = s.length(); // Length of the input string
+        int i = 0; // Starting index for the current group of characters
+
+        // Loop through the string until the end is reached
+        while (i < len) {
+            char ch = s[i]; // Current character
+            int start = i; // Starting index of the current group
+            int j = i + 1, count = 1; // Initialize j to the next character and count to 1
+
+            // Continue to the next characters while they are the same as the current character
+            while (j < len && s[j] == ch) {
                 j++;
                 count++;
             }
-            if(count >= 3)
-                ans.push_back({start, j-1});
-            i = j;
+
+            // If the group length is 3 or more, add the start and end indices to the result
+            if (count >= 3)
+                ans.push_back({start, j - 1});
+
+            i = j; // Move to the next new character
         }
 
-        return ans;
+        return ans; // Return the list of large group positions
     }
 };
