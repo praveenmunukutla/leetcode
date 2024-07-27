cd2dafb1516c57e13ab200d322296f1e7305c2ad
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-27 11:48:16 +0530

    Time: 11 ms (19.81%) | Memory: 19.3 MB (18.19%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -4,7 +4,7 @@ public:
         // Sort the array using a custom comparator
         std::sort(nums.begin(), nums.end(), [](auto a, auto b) {
             // Custom comparator: sorts by parity (even numbers before odd numbers)
-            return (a % 2) < (b % 2);
+            return a%2 < b%2;
         });
         // Return the sorted array
         return nums;
