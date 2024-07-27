42a1d26e3fc41a2ac62dab2d3e6ee8219ac9c8b0
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-27 20:50:58 +0530

    Time: 5 ms (31.47%) | Memory: 10.6 MB (5.59%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
+        // Make a copy of the heights vector
         vector<int> ch = heights;
+        
+        // Sort the copy
         sort(ch.begin(), ch.end());
+        
         int count = 0;
+        
+        // Compare each element of the original and sorted vectors
         for(int i = 0; i < heights.size(); i++){
+            // If elements at the same position are different, increment the count
             if(heights[i] != ch[i]) count++;
         }
+        
+        // Return the number of mismatches
         return count;
     }
 };
