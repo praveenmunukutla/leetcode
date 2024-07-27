08705f4b777dca644a3cf58b0cc1a7489ef67fc0
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-27 18:35:02 +0530

    Time: 15 ms (85.55%) | Memory: 25.1 MB (11.94%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
         int n = arr.size(), i = 0, j = n -1;
+        // Climb up the mountain from left
         while(i + 1 < n && arr[i] < arr[i+1])i++;
+        // Climb up the mountain from right
         while(j > 0 && arr[j-1] > arr[j])j--;
+        // meeting point should be same and not at boundaries.
         return i > 0 && i == j && j < n-1;
     }
 };
