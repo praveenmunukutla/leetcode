bb293dfb2b79cfae862f2fd284a1ec5c5ca24b2f
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-28 14:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (5.41%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -3,18 +3,23 @@ public:
     int bitwiseComplement(int n) {
         if(n == 0) return 1;
 
-        string ans = "";
+        int dec = 0;
+        int power = 1;
         while(n){
             int d = n&1;
             d = d ^ 1;
-            ans.insert(0, to_string(d));
+
+            if(d)
+                dec += power;
+            power = 2 * power;
+
             n = n >> 1;
         }
 
-        int dec = 0;
-        for(int i = 0; ans[i] != '\0'; i++){
-            dec = dec * 2 + ans[i]-'0';
-        }
+        // int dec = 0;
+        // for(int i = 0; ans[i] != '\0'; i++){
+        //     dec = dec * 2 + ans[i]-'0';
+        // }
 
         return dec;
     }
