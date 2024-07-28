80429e6bc7cc650de72af039943718a244f164c4
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-28 14:50:37 +0530

    Time: 3 ms (31.42%) | Memory: 7.5 MB (5.41%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
+        // Special case for n = 0, as its complement should be 1
         if(n == 0) return 1;
 
-        int dec = 0;
-        int power = 1;
+        int dec = 0;  // This will hold the decimal value of the complement
+        int power = 1;  // Power of 2 that will be used to construct the decimal value
+
+        // Loop until n becomes 0
         while(n){
-            int d = n&1;
+            // Get the least significant bit of n
+            int d = n & 1;
+            // Flip the bit
             d = d ^ 1;
 
+            // If the flipped bit is 1, add the current power of 2 to the decimal value
             if(d)
                 dec += power;
+
+            // Move to the next power of 2
             power = 2 * power;
 
+            // Right shift n to process the next bit
             n = n >> 1;
         }
 
-        // int dec = 0;
-        // for(int i = 0; ans[i] != '\0'; i++){
-        //     dec = dec * 2 + ans[i]-'0';
-        // }
-
+        // Return the decimal value of the bitwise complement
         return dec;
     }
 };
