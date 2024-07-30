8ff125a109ad8abd3e52ec5197e4552629377dfe
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-30 11:36:27 +0530

    Time: 3 ms (63.89%) | Memory: 9.8 MB (5.76%) - LeetSync

--- 2406-decode-the-message/decode-the-message.cpp
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -2,7 +2,6 @@ class Solution {
     unordered_map<char, char> umap;
 public:
     string decodeMessage(string key, string message) {
-        
         umap[' '] = ' ';
         for(int i = 0, index = 0; key[i] != '\0'; i++){
             if(umap.find(key[i]) == umap.end()){
