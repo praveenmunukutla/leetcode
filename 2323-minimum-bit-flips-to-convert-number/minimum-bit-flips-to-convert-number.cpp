97daa8043c2b2af3c9a3c153689b503ad0539447
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-30 21:11:24 +0530

    Time: 4 ms (43.03%) | Memory: 7.4 MB (24.68%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
+        // Perform XOR to find differing bits
         int xr = start ^ goal;
+        // Initialize count of bit flips
         int count = 0;
-        while(xr){
+        // Count the number of 1s in the XOR result
+        while (xr) {
             count++;
-            xr = xr & (xr-1);
+            // Clear the least significant bit set to 1
+            xr = xr & (xr - 1);
         }
+        // Return the total number of bit flips
         return count;
     }
 };
