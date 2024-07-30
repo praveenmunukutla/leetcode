4e8c622318d716a5b828acddbf62048dbd2d48d6
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-30 21:19:09 +0530

    Time: 3 ms (45.27%) | Memory: 7.6 MB (17.06%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
+        // Initialize the count of steps to 0
         int count = 0;
-        while(num){
-            num = num % 2 == 0 ? num/2 : num-1;
+        
+        // Loop until the number becomes 0
+        while (num) {
+            // If the number is even, divide it by 2
+            // If the number is odd, subtract 1
+            num = num % 2 == 0 ? num / 2 : num - 1;
+            
+            // Increment the step count for each operation
             count++;
         }
+        
+        // Return the total number of steps taken
         return count;
     }
 };
