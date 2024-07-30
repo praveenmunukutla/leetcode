fed77a4def63c4d6d7012f7f1bd91f3fb8d57f05
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-30 10:47:37 +0530

    Time: 38 ms (43.29%) | Memory: 27.9 MB (10.22%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -2,40 +2,19 @@
 #include <unordered_map>
 
 class Solution {
-    // Define an unordered_map where the key is an int and the value is a vector of vectors of ints
-    std::unordered_map<int, std::vector<std::vector<int>>> umap;
+    std::unordered_map<int, int> umap;
 public:
-      int speed = []() {
-        ios::sync_with_stdio(false); // Disable synchronization with C I/O 
-        cin.tie(NULL); // Untie cin from cout 
-        cout.tie(NULL); // Untie cout (though this is not necessary) 
-        return 0; 
-    }(); 
-
     int numEquivDominoPairs(std::vector<std::vector<int>>& dominoes) {
         // Iterate through each domino
         for (auto d : dominoes) {
-            int sum = d[0] + d[1]; // Calculate the sum of the two numbers in the domino
-            if (umap.find(sum) == umap.end()) 
-                umap[sum] = std::vector<std::vector<int>>(); // Initialize the vector if the sum is not found
-            umap[sum].push_back({d[0], d[1]}); // Add the domino to the vector corresponding to the sum
+            int sum = min(d[0],d[1])*10+max(d[0],d[1]);
+            umap[sum]++;
         }
 
         int count = 0; // Initialize a counter for equivalent domino pairs
         // Iterate through the unordered_map
         for (auto u : umap) {
-            if (u.second.size() > 1) { // Only consider vectors with more than one domino
-                std::vector<std::vector<int>> dom = u.second;
-                int len = u.second.size();
-                // Compare each pair of dominoes in the vector
-                for (int i = 0; i < len; i++) {
-                    for (int j = i + 1; j < len; j++) {
-                        // Check if the dominoes are equivalent
-                        if (dom[i][0] == dom[j][0] && dom[i][1] == dom[j][1]) count++;
-                        else if (dom[i][0] == dom[j][1] && dom[i][1] == dom[j][0]) count++;
-                    }
-                }
-            }
+            count += (u.second * (u.second-1)) / 2;
         }
 
         return count; // Return the total count of equivalent domino pairs
