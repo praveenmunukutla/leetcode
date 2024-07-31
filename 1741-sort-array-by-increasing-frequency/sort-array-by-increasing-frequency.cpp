ff985553a22449e242dd6b0f33ed799ba06c7cd4
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-07-31 10:23:12 +0530

    Time: 6 ms (59.01%) | Memory: 14.9 MB (62.51%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,14 +1,20 @@
 class Solution {
-    unordered_map<int, int> umap;
+    unordered_map<int, int> umap; // Unordered map to store the frequency of each number
 public:
     vector<int> frequencySort(vector<int>& nums) {
+        // Count the frequency of each number in the input vector
         for(auto n : nums) umap[n]++;
+        
+        // Sort the vector based on the custom comparator
         sort(nums.begin(), nums.end(), [&](auto& a, auto& b){
+            // If two numbers have the same frequency, sort them in descending order
             if(umap[a] == umap[b]){
                 return a > b;
             }
+            // Otherwise, sort them based on frequency in ascending order
             return umap[a] < umap[b];
         });
-        return nums;
+        
+        return nums; // Return the sorted vector
     }
 };
