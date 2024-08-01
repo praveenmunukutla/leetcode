bb1c65037a91c907d89b2322c5e654bf582dae67
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-01 09:03:55 +0530

    Time: 3 ms (37.89%) | Memory: 8.2 MB (52.37%) - LeetSync

---
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string getSmallestString(string s) {
+        int len = s.length();
+        for(int i = 0; i < len-1; i++){
+            int fi = s[i]-'0';
+            int se = s[i+1]-'0';
+            cout<<""<<fi<<" "<<se<<endl;
+            if((fi%2==1) && (se%2==1)){
+                //odd
+                if(fi > se){
+                    swap(s[i], s[i+1]);
+                    return s;
+                }
+
+            }else if((fi%2 == 0) && (se%2 == 0)) {
+                //even
+                if(fi > se)
+                {
+                    swap(s[i], s[i+1]);
+                    return s;
+                }
+            }
+        }
+        return s;
+    }
+};
