55b770a96b202efb2544313c8b344e27a09dce36
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-01 08:54:02 +0530

    Time: 4 ms (74.64%) | Memory: 26.5 MB (10.67%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.cpp
+++ 3463-alternating-groups-i/alternating-groups-i.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& c) {
-        int count = 0, len = c.size();
-        int f , s, t;
-        for(int i = 0; i < len; i++){
-            f = i;
-            s = (i+1)%len;
-            t = (i+2)%len;
-            if(c[f]==0 && c[s]==1 && c[t] == 0) count++;
-            else if(c[f]==1 && c[s]==0 && c[t] == 1) count++;
+        int count = 0;
+        int len = c.size();
+
+        // Iterate through each index in the vector
+        for(int i = 0; i < len; i++) {
+            // Calculate the indices for the next two elements
+            int s = (i + 1) % len;
+            int t = (i + 2) % len;
+
+            // Check for alternating groups of [0, 1, 0] or [1, 0, 1]
+            if (c[i] == 0 && c[s] == 1 && c[t] == 0) count++;
+            else if (c[i] == 1 && c[s] == 0 && c[t] == 1) count++;
         }
+        
         return count;
     }
 };
