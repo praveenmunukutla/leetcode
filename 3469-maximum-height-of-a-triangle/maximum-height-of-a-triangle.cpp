60869389ba4349f04ae92135be0cd23a67cb46ad
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-02 09:26:55 +0530

    Time: 3 ms (28.74%) | Memory: 14.4 MB (5.25%) - LeetSync

--- 3469-maximum-height-of-a-triangle/maximum-height-of-a-triangle.cpp
+++ 3469-maximum-height-of-a-triangle/maximum-height-of-a-triangle.cpp
@@ -1,17 +1,43 @@
+#include <map>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+
 class Solution {
-    int maxH = INT_MIN;
 public:
     int maxHeightOfTriangle(int red, int blue) {
-        return max(maxHeightOfTriangleUtil(red,blue,true,1), maxHeightOfTriangleUtil(red,blue,false,1));
+        memo.clear();
+        return max(Util(red, blue, true, 1), Util(red, blue, false, 1));
     }
 
-    int maxHeightOfTriangleUtil(int red, int blue, bool startWithRed, int level){
-        if(startWithRed){
-            if(red < level) return 0;
-            return 1 + maxHeightOfTriangleUtil(red-level, blue, !startWithRed, level+1);
+private:
+    // Custom key struct for the map
+    struct Key {
+        int red;
+        int blue;
+        bool startWithRed;
+        int level;
+
+        bool operator<(const Key& other) const {
+            // Define a strict weak ordering for the key
+            return tie(red, blue, startWithRed, level) < tie(other.red, other.blue, other.startWithRed, other.level);
+        }
+    };
+
+    map<Key, int> memo;
+
+    int Util(int red, int blue, bool startWithRed, int level) {
+        Key key = { red, blue, startWithRed, level };
+        if (memo.find(key) != memo.end()) {
+            return memo[key];
         }
 
-        if(blue < level) return 0;
-        return 1 + maxHeightOfTriangleUtil(red, blue-level, !startWithRed, level+1);
+        if (startWithRed) {
+            if (red < level) return memo[key] = 0;
+            return memo[key] = 1 + Util(red - level, blue, !startWithRed, level + 1);
+        }
+
+        if (blue < level) return memo[key] = 0;
+        return memo[key] = 1 + Util(red, blue - level, !startWithRed, level + 1);
     }
 };
