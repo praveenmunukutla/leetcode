58e4f64f5bd5080b3ffcb715e6f19d04aa9a6126
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-02 10:50:39 +0530

    Time: 2 ms (46.41%) | Memory: 8 MB (48.20%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,16 +1,11 @@
 class Solution {
-    stack<char> st;
 public:
     string clearDigits(string s) {
-        for(auto c : s)
-            (c >= '0' && c <='9') ? st.pop() : st.push(c);
-
-        string ans="";
-        while(!st.empty()){
-            ans.insert(ans.begin(), st.top());
-            st.pop();
+        string ans = "";
+        for(auto c : s){
+            if(isdigit(c)) ans.pop_back();
+            else ans += c;
         }
-
         return ans;
     }
 };
