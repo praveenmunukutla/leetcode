8dacf1dc1440536ec09f2a1b4ed53131ff5a24fe
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-02 11:00:46 +0530

    Time: 5 ms (25.34%) | Memory: 8.7 MB (6.83%) - LeetSync

--- 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -1,16 +1,29 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
+    // Function to find the minimum number of chairs required at any time
     int minimumChairs(string s) {
-        int maxChairs = 0;
-        int count = 0;
+        int maxChairs = 0;  // Variable to keep track of the maximum number of chairs needed
+        int count = 0;     // Variable to keep track of the current number of occupied chairs
+
+        // Iterate through each character in the string
         for(auto c : s){
-            if(c == 'E'){
+            if(c == 'E') {
+                // When encountering 'E', a new chair is occupied
                 count++;
-            }else if(c == 'L'){
+            } else if(c == 'L') {
+                // When encountering 'L', a chair is freed
                 count--;
             }
+
+            // Update maxChairs with the maximum value between the current maxChairs and count
             maxChairs = max(maxChairs, count);
         }
+
+        // Return the maximum number of chairs needed at any point in time
         return maxChairs;
     }
 };
