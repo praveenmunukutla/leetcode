cd2757cc4b0ff994ee8b19791517f5e041b85b30
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-02 11:13:30 +0530

    Time: 20 ms (6.70%) | Memory: 29 MB (6.70%) - LeetSync

---
+++ 3415-check-if-grid-satisfies-conditions/check-if-grid-satisfies-conditions.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int row, col;
+public:
+    bool isValid(vector<vector<int>>& grid, int x, int y){
+        if(x+1 < row){
+            if(grid[x][y] !=grid[x+1][y]) return false;
+        }
+
+        if(y+1 < col){
+            if(grid[x][y] == grid[x][y+1]) return false;
+        }
+
+        return true;
+    }
+
+    bool satisfiesConditions(vector<vector<int>>& grid) {
+        row = grid.size(), col = grid[0].size();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(!isValid(grid, i,j)) return false;
+            }
+        }
+
+        return true;
+    }
+};
