a101d96184d97b80b2a546049e659ba68b3c3d47
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-02 10:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (48.20%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
+    // string clearDigits(string s) {
+    //     string ans = "";
+    //     for(auto c : s){
+    //         if(isdigit(c)) ans.pop_back();
+    //         else ans += c;
+    //     }
+    //     return ans;
+    // }
+
     string clearDigits(string s) {
-        string ans = "";
-        for(auto c : s){
-            if(isdigit(c)) ans.pop_back();
-            else ans += c;
+        int i = 0, j = 0;
+        while(s[i] != '\0'){
+            if(!isdigit(s[i])) s[j++] = s[i];
+            else j--;
+            i++;
         }
-        return ans;
+        return s.substr(0,j);
     }
 };
