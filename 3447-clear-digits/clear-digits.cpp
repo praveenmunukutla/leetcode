ec50af560d1ad13cb7bd2b6178ba9e0a2d25d653
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-02 10:54:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.44%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -10,12 +10,20 @@ public:
     // }
 
     string clearDigits(string s) {
-        int i = 0, j = 0;
-        while(s[i] != '\0'){
-            if(!isdigit(s[i])) s[j++] = s[i];
-            else j--;
-            i++;
+        int i = 0, j = 0;  // Initialize two pointers
+
+        // Iterate through the string using pointer i
+        while (s[i] != '\0') {
+            // If the current character is not a digit, place it at position j
+            if (!isdigit(s[i])) {
+                s[j++] = s[i];  // Copy non-digit character to the current position of j
+            } else {
+                j--;  // Remove the last non-digit character by decrementing j
+            }
+            i++;  // Move to the next character
         }
-        return s.substr(0,j);
+
+        // Return the substring from the start to the last valid position
+        return s.substr(0, j);
     }
 };
