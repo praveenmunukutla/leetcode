0741643f51451049cdbb76e8d2dece25bce904d3
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-02 11:07:36 +0530

    Time: 14 ms (8.75%) | Memory: 27.2 MB (9.76%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,11 +1,21 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
+    // Function to check if the array is "special"
     bool isArraySpecial(vector<int>& nums) {
-        for(int i = 0; i < nums.size()-1;i++){
-            bool n1 = nums[i]%2 == 0;
-            bool n2 = nums[i+1]%2 == 0;
+        // Iterate through the array, stopping at the second-to-last element
+        for(int i = 0; i < nums.size() - 1; i++) {
+            // Check if the current element is even
+            bool n1 = nums[i] % 2 == 0;
+            // Check if the next element is even
+            bool n2 = nums[i + 1] % 2 == 0;
+            
+            // If both elements are either even or odd, the array is not "special"
             if(n1 == n2) return false;
         }
+        // If all adjacent elements have different parity, the array is "special"
         return true;
     }
 };
