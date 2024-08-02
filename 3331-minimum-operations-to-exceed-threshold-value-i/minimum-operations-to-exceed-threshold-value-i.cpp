6858ff6b672e5c7db624109488ecb7eeeb80ee0f
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-02 18:19:09 +0530

    Time: 3 ms (89.40%) | Memory: 25.5 MB (12.73%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
+        // Create a min-heap (priority queue) from the input vector
         priority_queue<int, vector<int>, greater<int>> minpq(nums.begin(), nums.end());
 
-        int count = 0;
-        while(minpq.top() < k){
-            minpq.pop();
-            count++;
+        int count = 0; // Initialize operation counter
+
+        // While the smallest element in the heap is less than k
+        while(minpq.top() < k) {
+            minpq.pop(); // Remove the smallest element
+            count++; // Increment the operation counter
         }
 
-        return count;
+        return count; // Return the number of operations performed
     }
 };
