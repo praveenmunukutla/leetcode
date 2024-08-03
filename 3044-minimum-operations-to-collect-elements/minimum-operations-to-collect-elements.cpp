cecf9ae4492b63aca3ebca2a7bd590af0f6da6a5
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-03 15:25:18 +0530

    Time: 3 ms (78.01%) | Memory: 22.7 MB (10.84%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    unordered_set<int> uset;
+    unordered_set<int> uset; // Unordered set to keep track of unique numbers from 1 to k
 public:
     int minOperations(vector<int>& nums, int k) {
-        int count = 0;
-        for(int i = nums.size()-1; i >= 0; i--){
-            if(uset.size() == k) return count;
-            if(nums[i] <= k) {
-                uset.insert(nums[i]);
+        int count = 0; // Counter to keep track of operations
+        for(int i = nums.size()-1; i >= 0; i--){ // Iterate from the end of the array to the beginning
+            if(uset.size() == k) return count; // If the set contains all numbers from 1 to k, return the count
+            if(nums[i] <= k) { // If the current number is less than or equal to k
+                uset.insert(nums[i]); // Add the number to the set
             }
-            count++;
+            count++; // Increment the operation count
         }
-        return count;
+        return count; // Return the total number of operations if the set size never reaches k
     }
 };
