cc2c87c505ee4f9d18c922007fc4dbde80a4e529
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-03 16:07:20 +0530

    Time: 11 ms (90.70%) | Memory: 21.9 MB (36.38%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int index = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0){
-                nums[index++] = nums[i];
+        int index = 0; // Initialize index to keep track of the position for the next non-zero element
+        
+        // First pass: Move all non-zero elements to the beginning of the vector
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) { // If the current element is not zero
+                nums[index++] = nums[i]; // Place it at the current index and increment the index
             }
         }
-        for(int i = index; i < nums.size() ; i++)
-            nums[i] = 0;
+        
+        // Second pass: Fill the remaining positions in the vector with zeroes
+        for(int i = index; i < nums.size(); i++) {
+            nums[i] = 0; // Set the element at the current position to zero
+        }
     }
 };
