953ef15424a1ccc7a554bd3bc8ae14e418dfe23b
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-07 12:15:12 +0530

    Time: 7 ms (56.31%) | Memory: 28.1 MB (9.31%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        unordered_map<int,int> umap;
+        unordered_map<int,int> umap; // Unordered map to store the frequency of each element in nums
         for(auto a : nums)
-            umap[a]++;
+            umap[a]++; // Increment the frequency count for each element
 
-        int size = nums.size()-1;        
-        for(int i = 1; i < size; i++){
-            if(umap[i] != 1) return false;
+        int size = nums.size()-1; // Calculate the size-1 of the nums vector
+        for(int i = 1; i < size; i++){ // Loop through numbers from 1 to size-1
+            if(umap[i] != 1) return false; // Check if each number from 1 to size-1 appears exactly once
         }
 
-        if(umap[size] != 2) return false;
+        if(umap[size] != 2) return false; // Check if the number equal to size appears exactly twice
 
-        return true;
+        return true; // Return true if all conditions are met
     }
 };
