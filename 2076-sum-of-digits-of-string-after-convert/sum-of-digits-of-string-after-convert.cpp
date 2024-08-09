1e8165ba71df4217adbb1d9a615b10ff7a24e293
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-09 07:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (24.07%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int getLucky(string s, int k) {
         string num = "";
-        for(auto s1 : s)
-             num += to_string((s1-'a')+1);
+        
+        // Convert each character to its corresponding numerical value in the alphabet.
+        for(auto c : s)
+            num += to_string((c - 'a') + 1);
 
-        int sum;
-        while(k){
-            sum = 0;
-            string numd = "";
-            for(auto n : num){
-                sum += (n-'0');
-            }
+        // Iterate k times, transforming the number by summing its digits.
+        while (k--) {
+            int sum = 0;
+            for (auto digit : num)
+                sum += (digit - '0');  // Convert character digit to integer and sum
 
-            num = to_string(sum);
-            k--;
+            num = to_string(sum);  // Convert the sum back to a string for the next iteration
         }
 
-        return sum;        
+        // The final result is stored in `num`, so convert it to an integer and return.
+        return stoi(num);
     }
 };
