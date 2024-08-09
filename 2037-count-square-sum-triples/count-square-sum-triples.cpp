7e4ba5e258d44888ff7ad98c6fd408874ecdc1f6
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-09 07:04:13 +0530

    Time: 11 ms (51.30%) | Memory: 7.3 MB (42.21%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -1,17 +1,13 @@
 class Solution {
-    unordered_set<int> uset;
 public:
     int countTriples(int n) {
-        for(int i = 1; i <= n; i++) uset.insert(i*i);
-
         int count = 0;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
                 if(i == j) continue;
                 auto sum = (i*i) + (j*j);
-                if(uset.count(sum)){
-                    count++;
-                }
+                auto sroot = (int)sqrt(sum);
+                if(sroot <= n && sroot*sroot == sum) count++;
             }
         }
 
