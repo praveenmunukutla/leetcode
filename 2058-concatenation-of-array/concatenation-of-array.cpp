b5ec9b7f8ad469c2af2d962bec7b6e27bc69d35e
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-09 07:09:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (14.79%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
         int size = nums.size();
         for(int i = 0; i < size; i++)
-            nums.push_back(nums[i]);
+            nums.emplace_back(nums[i]);
         return nums;
     }
 };
