176183853f1fec4753ddcb409b2e71c5f903c0ab
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-09 07:42:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (55.38%) - LeetSync

--- 2083-three-divisors/three-divisors.cpp
+++ 2083-three-divisors/three-divisors.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool isThree(int n) {
-        int count  = 0;
-        for(int i = 1; i * i <= n; i++){
-            if(n % i == 0 && i*i == n) count++;
-            else if(n % i == 0) count+=2;
+        int count = 0;
+        
+        for(int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                count++;  // Count divisor `i`
+                if(i != n / i) count++;  // Count the corresponding divisor `n / i`
+            }
         }
+        
         return count == 3;
     }
 };
