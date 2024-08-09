de298db933aaaf6cf282834bcc49383e43b22f15
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-09 07:07:07 +0530

    Time: 7 ms (73.59%) | Memory: 7.3 MB (50.87%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -3,11 +3,10 @@ public:
     int countTriples(int n) {
         int count = 0;
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(i == j) continue;
+            for(int j = i+1; j <= n; j++){
                 auto sum = (i*i) + (j*j);
                 auto sroot = (int)sqrt(sum);
-                if(sroot <= n && sroot*sroot == sum) count++;
+                if(sroot <= n && sroot*sroot == sum) count+=2;
             }
         }
 
