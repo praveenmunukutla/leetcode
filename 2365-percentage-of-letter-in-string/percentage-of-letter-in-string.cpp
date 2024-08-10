f70e3602c45e4cac6214fc6938ff6d8672d2ec99
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-10 22:43:05 +0530

    Time: 3 ms (45.43%) | Memory: 7.8 MB (8.87%) - LeetSync

--- 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.cpp
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-    int percentageLetter(string s, char letter) {
+    // Function to calculate the percentage of a specific letter in the string
+    int percentageLetter(std::string s, char letter) {
+        // Get the length of the input string
         int len = s.length();
+        
+        // Initialize a counter to count occurrences of the specified letter
         int count = 0;
+
+        // Loop through each character in the string
         for(auto c : s) {
+            // If the current character matches the specified letter, increment the count
             if(c == letter) 
                 count++;
         }
 
-        return floor(((double)count/len)*100);
+        // Calculate the percentage of the specified letter in the string
+        // Convert the count and length to double for accurate division
+        // Multiply by 100 to get the percentage
+        // Use floor to round down the result to the nearest integer
+        return std::floor(((double)count / len) * 100);
     }
 };
