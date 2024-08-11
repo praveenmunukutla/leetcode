f1a2cd6ac9e972f147e7c071b282f2434801e330
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-11 14:42:31 +0530

    Time: 247 ms (5.12%) | Memory: 118.4 MB (5.61%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,18 +1,21 @@
 class Solution {
-    unordered_map<int, vector<int>> umap;
+    unordered_map<int, vector<int>> umap;  // Adjacency list representation of the graph
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size()+1;
-        for(auto e : edges){
+        int n = edges.size() + 1;  // Total number of nodes in the graph
+        
+        // Build the adjacency list
+        for(auto e : edges) {
             umap[e[0]].push_back(e[1]);
             umap[e[1]].push_back(e[0]);
         }
 
-        for(auto u : umap){
-            if(u.second.size() == n-1)
+        // Find the center node (the node connected to all other nodes)
+        for(auto u : umap) {
+            if(u.second.size() == n - 1)  // If a node is connected to n-1 other nodes, it's the center
                 return u.first;
         }
 
-        return -1;        
+        return -1;  // Return -1 if no center is found (should not happen in a valid star graph)
     }
 };
