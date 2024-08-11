c693efa57e8becff01e70c5ef076bac448a4355d
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-11 09:34:14 +0530

    Time: 47 ms (61.43%) | Memory: 37.1 MB (44.54%) - LeetSync

--- 1708-design-parking-system/design-parking-system.cpp
+++ 1708-design-parking-system/design-parking-system.cpp
@@ -1,21 +1,24 @@
 class ParkingSystem {
-    unordered_map<int,int> umap;
+    unordered_map<int, int> umap;  // Map to store the number of available parking spots for each car type
+
 public:
+    // Constructor to initialize parking spots for each car type
     ParkingSystem(int big, int medium, int small) {
-        umap[3]=small;
-        umap[2]=medium;
-        umap[1]=big;
+        umap[3] = small;   // Small car spots
+        umap[2] = medium;  // Medium car spots
+        umap[1] = big;     // Big car spots
     }
     
+    // Attempt to add a car of given type to the parking system
     bool addCar(int carType) {
-        if(umap[carType] <= 0) return false;
-        umap[carType]--;
-        return true;
+        if (umap[carType] <= 0) return false;  // Check if there's available space
+        umap[carType]--;  // Decrease the count of available spots
+        return true;  // Successfully parked the car
     }
 };
 
 /**
- * Your ParkingSystem object will be instantiated and called as such:
+ * Example usage:
  * ParkingSystem* obj = new ParkingSystem(big, medium, small);
  * bool param_1 = obj->addCar(carType);
  */
