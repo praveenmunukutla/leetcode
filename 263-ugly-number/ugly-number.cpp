f47e09f3c2d53218c5fdba3c689c5297e7ac5f22
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-12 09:07:12 +0530

    Time: 2 ms (39.02%) | Memory: 7.5 MB (35.22%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -3,15 +3,9 @@ public:
     bool isUgly(int n) {
         if(n <= 0) return false;
 
-        while(n%2 == 0){
-            n = n/2;
-        }
-        while(n%3 == 0){
-            n = n/3;
-        }
-        while(n%5 == 0){
-            n = n/5;
-        }
+        while(n%2 == 0)n = n/2;
+        while(n%3 == 0)n = n/3;
+        while(n%5 == 0)n = n/5;
 
         return n==1;
     }
