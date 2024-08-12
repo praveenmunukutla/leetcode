ad6c9e878e407722d0133f84034026b40aa0fac6
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-12 11:11:24 +0530

    Time: 4 ms (36.41%) | Memory: 7.4 MB (38.78%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
-        for(int i = 0; i < 32; i++){
-            ans <<= 1;
-            ans |= (n&1);
-            n >>= 1;
+        for (int i = 0; i < 32; ++i) {
+            ans <<= 1; // Make space for the next bit
+            ans |= (n & 1); // Set the last bit of ans to the last bit of n
+            n >>= 1; // Shift n to the right to process the next bit
         }
         return ans;
     }
