bc9192a4b7e2efb327f8bf6b325caad5a219057b
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-13 21:35:45 +0530

    Time: 3 ms (43.17%) | Memory: 7.7 MB (20.64%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -1,25 +1,14 @@
 class Solution {
-    unordered_map<int, vector<int>> umap;
 public:
     char repeatedCharacter(string s) {
-        for(int i = 0; i < s.length(); i++){
-            umap[s[i]].push_back(i);
-        }
-
-        char ans = ' ';
-        int sIndex = -1;
-        for(auto u : umap){
-            if(u.second.size() > 1){
-                if(sIndex == -1){
-                    ans = u.first;
-                    sIndex = u.second[1];
-                }else if(u.second[1] < sIndex){
-                    ans = u.first;
-                    sIndex = u.second[1];
-                }
+        unordered_map<char, int> umap;
+        for(int i = 0; i < s.length(); i++) {
+            // Check if the character has been seen before
+            if(umap.find(s[i]) != umap.end()) {
+                return s[i];  // First repeated character
             }
+            umap[s[i]] = i;  // Store the index of the character
         }
-
-        return ans;
+        return ' ';  // Default return, although problem guarantees there is a repeated character
     }
 };
