955d5702fc37cb721ea3aa4d164a577b2da47240
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-15 15:47:38 +0530

    Time: 2 ms (55.99%) | Memory: 9.9 MB (6.08%) - LeetSync

---
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    queue<pair<int,int>> q;
+public:
+    vector<int> shortestToChar(string s, char c) {
+
+        int len = s.length();
+        vector<int> dist = vector<int>(len, INT_MAX);
+        for(int i = 0; i < len; i++){
+            if(s[i] == c){
+                q.push({i,0});
+            }
+        }
+
+        while(!q.empty()){
+            auto c = q.front();q.pop();
+            if(c.second < dist[c.first]){
+                dist[c.first] = c.second;
+                int left = c.first-1;
+                int right = c.first+1;
+                if(left >= 0){
+                    q.push({left, c.second+1});
+                }
+                if(right < len){
+                    q.push({right, c.second+1});
+                }
+            }
+        }
+
+        return dist;
+    }
+};
