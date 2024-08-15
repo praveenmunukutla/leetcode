39130d74ffa17d8129b26ec6be3d7c73473d693d
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-15 08:29:54 +0530

    Time: 206 ms (48.46%) | Memory: 120 MB (56.33%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -3,6 +3,7 @@ public:
     int distributeCandies(vector<int>& candyType) {
         unordered_set<int> uset(candyType.begin(), candyType.end());
         int nby2 = candyType.size()/2;
-        return nby2 < uset.size() ? nby2 : uset.size();
+        int usize = uset.size();
+        return min(nby2, usize);
     }
 };
