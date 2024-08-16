063c6639640478887f21d6f420717a5863d33906
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-16 12:53:14 +0530

    Time: 26 ms (76.70%) | Memory: 28.6 MB (81.17%) - LeetSync

---
+++ 697-degree-of-an-array/degree-of-an-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    unordered_map<int, tuple<int,int,int>> umap;
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            if(umap.find(nums[i]) == umap.end()){
+                umap[nums[i]] = {1, i, -1};
+            }else{
+                auto& [f,fi,si] = umap[nums[i]];
+                f++;
+                si = i;
+            }
+        }
+        
+        int minSub = INT_MAX;
+        int maxFreq = INT_MIN;
+
+        for(auto u : umap){
+            auto [f,fi,si] = u.second;
+            if(f > maxFreq){
+                minSub = INT_MAX;
+                auto sasize = si == -1 ? f : si-fi+1;
+                minSub = min(minSub, sasize);       
+                maxFreq = max(maxFreq, f);
+            }else if(f == maxFreq){
+                auto sasize = si == -1 ? f : si-fi+1;
+                minSub = min(minSub, sasize);
+            }
+        }
+        
+        return minSub;
+    }
+};
