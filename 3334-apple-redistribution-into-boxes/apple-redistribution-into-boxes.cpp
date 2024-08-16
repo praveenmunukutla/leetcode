26c4dfbe74910538e8345ebc30733b6bbfe1c5e3
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-16 16:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (14.75%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        // Calculate the total number of apples
         auto s = accumulate(apple.begin(), apple.end(), 0);
+        
+        // Create a max-heap (priority_queue) from the capacity array
         priority_queue<int> q(capacity.begin(), capacity.end());
+        
+        // Initialize the count of boxes used to 0
         int count = 0;
 
+        // Process boxes while there are still boxes in the priority queue
         while(!q.empty()){
-            auto v = q.top(); q.pop();
+            // Get the box with the largest capacity
+            auto v = q.top(); 
+            q.pop();
+            
+            // Subtract the capacity of the current box from the total apples
             s = s - v;
+            
+            // Increment the count of boxes used
             count++;
 
+            // If all apples are accommodated, exit the loop
             if(s <= 0) break;
         }
 
+        // Return the total number of boxes used
         return count;
     }
 };
