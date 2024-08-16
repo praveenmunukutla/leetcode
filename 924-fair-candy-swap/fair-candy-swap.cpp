3fa9f594c13b2ad5fe7598ee51558a4654a728a1
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-16 16:16:47 +0530

    Time: 104 ms (25.64%) | Memory: 62.5 MB (9.68%) - LeetSync

---
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -0,0 +1,20 @@
+class Solution {
+
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+        unordered_set<int> alices(aliceSizes.begin(),aliceSizes.end());
+        unordered_set<int> bobs(bobSizes.begin(),bobSizes.end());
+
+        auto as = accumulate(aliceSizes.begin(), aliceSizes.end(), 0);
+        auto bs = accumulate(bobSizes.begin(), bobSizes.end(), 0);
+        auto delta  = (bs-as)/2;
+
+        for(auto a : aliceSizes){
+            if(bobs.count(a+delta)){
+                return {a, a+delta};
+            }
+        }
+
+        return {0,0};
+    }
+};
