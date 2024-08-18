017394a47649f6ed5efff9dc4d30180d8429cac9
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-18 12:57:58 +0530

    Time: 4 ms (78.38%) | Memory: 25.3 MB (68.77%) - LeetSync

--- 3164-last-visited-integers/last-visited-integers.cpp
+++ 3164-last-visited-integers/last-visited-integers.cpp
@@ -4,13 +4,16 @@ public:
         vector<int> ans;
         vector<int> seen;
         int index = -1;
-        for(auto n : nums){
-            if(n == -1){
-               if(index < 0) ans.push_back(-1);
-               else ans.push_back(seen[index--]);
-            }else{
+
+        for (auto n : nums) {
+            if (n == -1) {
+                // Backtrack: Push the last seen integer or -1 if none available
+                if (index < 0) ans.push_back(-1);
+                else ans.push_back(seen[index--]);
+            } else {
+                // Store the current integer and update the index
                 seen.push_back(n);
-                index = seen.size()-1;
+                index = seen.size() - 1;
             }
         }
         return ans;
