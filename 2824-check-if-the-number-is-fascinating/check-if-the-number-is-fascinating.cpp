590383c7d6de726d5db413e78251b9b626879a6f
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-18 11:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (44.99%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.cpp
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.cpp
@@ -2,15 +2,8 @@ class Solution {
 public:
     bool isFascinating(int n) {
         string s = to_string(n) + to_string(n*2) + to_string(n*3);
-        vector<int> check(10,0);
-        for(auto c : s){
-            check[c-'0']++;
-        }
-
-        if(check[0]) return false;
-        for(int i = 1; i < 10;i++){
-            if(check[i] != 1) return false;
-        }
-        return true;
+        if(s.length() > 9) return false;
+        sort(s.begin(), s.end());
+        return s == "123456789";
     }
 };
