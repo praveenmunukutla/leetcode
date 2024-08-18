e6010db779ffc128d90b0fe58da2e68f2638c466
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-18 14:24:25 +0530

    Time: 13 ms (39.39%) | Memory: 29.5 MB (21.21%) - LeetSync

--- 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.cpp
+++ 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    int findKOr(vector<int>& nums, int k) {   
-        vector<int> res(32,0);
-        for(int i = 0; i < 32; i++){
+    int findKOr(vector<int>& nums, int k) {
+        vector<int> res(32, 0);
+
+        for (int i = 0; i < 32; ++i) {
             int count = 0;
-            for(auto& n : nums){
-                auto bit = n & 1;
-                n = n >> 1;
-                if(bit) count++;
-                if(count >= k) {res[i] = 1;}                
+            for (int n : nums) {
+                if (n & (1 << i)) { // Check if the ith bit is set
+                    ++count;
+                }
+                if (count >= k) {
+                    res[i] = 1; // Mark the ith bit as set
+                    break; // No need to check further numbers for this bit position
+                }
             }
         }
 
         int ans = 0;
-        for(int i = 0; i < 32; i++){
-            if(res[i])
-                ans += (1<<i);
+        for (int i = 0; i < 32; ++i) {
+            if (res[i]) {
+                ans |= (1 << i); // Construct the final result
+            }
         }
 
         return ans;
-        
     }
 };
