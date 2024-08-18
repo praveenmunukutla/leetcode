b6397be5b2aed488a3039e8366cfbcedc72767c3
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-18 14:19:54 +0530

    Time: 8 ms (73.48%) | Memory: 29.6 MB (21.21%) - LeetSync

---
+++ 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findKOr(vector<int>& nums, int k) {   
+        vector<int> res(32,0);
+        for(int i = 0; i < 32; i++){
+            int count = 0;
+            for(auto& n : nums){
+                auto bit = n & 1;
+                n = n >> 1;
+                if(bit) count++;
+                if(count >= k) {res[i] = 1;}                
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            ans += pow(2,i)*res[i];
+        }
+
+        return ans;
+        
+    }
+};
