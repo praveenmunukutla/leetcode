b1d6c8decd49f9e0d9a63b1e8637ce584df379cf
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-18 11:17:08 +0530

    Time: 7 ms (60.58%) | Memory: 28.5 MB (20.56%) - LeetSync

---
+++ 3246-check-if-bitwise-or-has-trailing-zeros/check-if-bitwise-or-has-trailing-zeros.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasTrailingZeros(vector<int>& nums) {
+        int count = 0;
+        for(auto n : nums){
+            if(n%2 == 0) count++;
+            if(count >= 2) return true;
+        }
+        return false;
+    }
+};
