bfe7d639dfaa7adc44fe80e4ce3cb69de8575427
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-20 13:04:29 +0530

    Time: 6 ms (27.87%) | Memory: 9.4 MB (8.34%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -6,15 +6,17 @@ public:
         // b - 1 ; a - 1; l - 2; o - 2; n - 1;
         string balloon = "balloon";
         int count = 0, i = 0;
-        while(true){
-            for(i = 0; balloon[i] != '\0'; i++){
-                char c = balloon[i];
-                if(umap[c] > 0) umap[c]--;
-                else break;
-            }
-            if(balloon[i] == '\0') count++;
-            else break;
-        }
-        return count;
+        // while(true){
+        //     for(i = 0; balloon[i] != '\0'; i++){
+        //         char c = balloon[i];
+        //         if(umap[c] > 0) umap[c]--;
+        //         else break;
+        //     }
+        //     if(balloon[i] == '\0') count++;
+        //     else break;
+        // }
+        int ban = min(umap['b'], min(umap['a'], umap['n']));
+        int lo = min(umap['l']/2, umap['o']/2);
+        return min(ban, lo);
     }
 };
