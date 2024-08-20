426864f6e154050f990f77e8e8eec9186390241d
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-20 13:42:08 +0530

    Time: 54 ms (35.21%) | Memory: 35.9 MB (54.95%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        vector<vector<int>> res;
-        sort(arr.begin(), arr.end());
-        int minAD = INT_MAX;
-        for(int i = 1; i < arr.size(); i++){
-            int mv = abs(arr[i]-arr[i-1]);
-            if(mv < minAD){
-                res.clear();
-                res.push_back({arr[i-1], arr[i]});
-                minAD = mv;
-            }else if(mv == minAD){
-                res.push_back({arr[i-1], arr[i]});
+        vector<vector<int>> res; // Resulting pairs
+        sort(arr.begin(), arr.end()); // Sort the array
+        int minAD = INT_MAX; // Initialize minimum absolute difference
+        
+        for(int i = 1; i < arr.size(); i++) {
+            int mv = abs(arr[i] - arr[i-1]); // Calculate absolute difference
+            
+            if(mv < minAD) { 
+                res.clear(); // New minimum found, clear previous results
+                res.push_back({arr[i-1], arr[i]}); // Add new pair
+                minAD = mv; // Update minimum difference
+            } else if(mv == minAD) {
+                res.push_back({arr[i-1], arr[i]}); // Add pair with same minimum difference
             }
         }
         
-        return res;
+        return res; // Return all pairs with minimum absolute difference
     }
 };
