34fe883d08036bf211d89f53db6bf6e7bbb09216
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-22 09:02:44 +0530

    Time: 43 ms (7.08%) | Memory: 23.9 MB (5.20%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -3,6 +3,7 @@ public:
     int minDeletionSize(vector<string>& strs) {
         vector<vector<char>> mat;
         
+        // Convert each string into a vector of characters and store it in `mat`
         for(auto str : strs){
             vector<char> v;
             for(auto c : str)
@@ -10,19 +11,21 @@ public:
             mat.push_back(v);
         }
 
-        int row = mat.size();
-        int col = mat[0].size();
+        int row = mat.size();      // Number of rows in the matrix (number of strings)
+        int col = mat[0].size();   // Number of columns in the matrix (length of the strings)
 
         int count = 0;
+        // Iterate over each column
         for(int j = 0; j < col; j++){
+            // Check if the column is sorted in non-decreasing order
             for(int i = 1; i < row; i++){
-                if(mat[i][j] < mat[i-1][j]) {
-                    count++;
-                    break;
+                if(mat[i][j] < mat[i-1][j]) {  // If current element is smaller than the previous row element in the same column
+                    count++;                    // Increment the count of columns to be deleted
+                    break;                      // Move to the next column since this one is unsorted
                 }
             }
         }
 
-        return count;
+        return count;  // Return the number of columns that need to be deleted
     }
 };
