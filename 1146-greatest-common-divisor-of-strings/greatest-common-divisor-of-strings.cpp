9ace4d201e362053e577a04a865dd50b8504d3e3
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-22 08:55:44 +0530

    Time: 2 ms (67.92%) | Memory: 8.7 MB (26.49%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1+str2 != str2+str1) return "";
-
-        auto g = gcd(str1.length(),str2.length());
+        // Check if concatenation of the strings in both orders results in the same string
+        if (str1 + str2 != str2 + str1) return "";  // If not, return an empty string
+        
+        // Calculate the GCD of the lengths of the two strings
+        auto g = gcd(str1.length(), str2.length());
+        
+        // Return the substring of str2 that corresponds to the GCD length
         return str2.substr(0, g);
     }
 
-    int gcd(int a, int b){
-        if(a == 0) return b;
-        
-        return gcd(b%a, a);
+    // Helper function to calculate GCD using the Euclidean algorithm
+    int gcd(int a, int b) {
+        if (a == 0) return b;
+        return gcd(b % a, a);
     }
 };
