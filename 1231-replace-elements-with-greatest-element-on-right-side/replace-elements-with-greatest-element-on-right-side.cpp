e53ca64514ebff3943b6e0c3bf37a464e14547fe
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-23 09:58:13 +0530

    Time: 63 ms (90.47%) | Memory: 71.3 MB (20.52%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         int len = arr.size();
-        vector<int> maxEle(len, 0);
-        maxEle[len-1]=-1;
+        vector<int> maxEle(len, 0); // Initialize result vector with size 'len' and all elements set to 0
+        maxEle[len-1] = -1; // Last element should be -1 as per the problem statement
 
-        int maxVal = arr[len-1];
-        maxEle[len-1] = -1;
-        for(int i = len-2; i >= 0; i--){
-            maxEle[i] = maxVal;
-            maxVal = max(maxVal, arr[i]);
+        int maxVal = arr[len-1]; // Start with the last element as the initial max
+        for(int i = len-2; i >= 0; i--){ // Traverse from the second last element to the first
+            maxEle[i] = maxVal; // Set the current position to the maxVal found so far
+            maxVal = max(maxVal, arr[i]); // Update maxVal with the maximum of maxVal and current element
         }
 
-        return maxEle;
+        return maxEle; // Return the modified array
     }
 };
