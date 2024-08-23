6688430efae1ceb3a9656c596f74d7134511f14e
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-23 08:22:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (18.78%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> ans;
-        ans.reserve(n);
+        ans.reserve(n); // Reserve space for n elements
 
-        for(int i = 1; i <= n/2; i++){
-            ans.push_back(i);
-            ans.push_back(i*-1);
+        for(int i = 1; i <= n / 2; i++) {
+            ans.push_back(i);     // Add positive number
+            ans.push_back(-i);    // Add negative counterpart
         }
 
-        if(n%2 == 1)
-            ans.push_back(0);
+        if(n % 2 == 1) {
+            ans.push_back(0);    // Add 0 if n is odd
+        }
 
         return ans;
     }
