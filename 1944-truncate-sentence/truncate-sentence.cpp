074222e74808001dadebaf47ebb6b9946f63d11c
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 20:41:24 +0530

    Time: 4 ms (34.43%) | Memory: 8 MB (51.13%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.cpp
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
+    // string truncateSentence(string s, int k) {
+    //     auto tokens = [](string s){
+    //         stringstream ss(s);
+    //         string token;
+    //         vector<string> tokens;
+    //         while(getline(ss,token,' ')){
+    //             if(!token.empty())
+    //                 tokens.push_back(token);                    
+    //         }
+    //         return tokens;
+    //     };
+    //     string ans;
+    //     for(auto t : tokens(s)){
+    //         if(k == 0) break;
+    //         ans += t + " ";
+    //         k--;
+    //     }
+    //     ans.pop_back();
+    //     return ans;
+    // }
     string truncateSentence(string s, int k) {
-        auto tokens = [](string s){
-            stringstream ss(s);
-            string token;
-            vector<string> tokens;
-            while(getline(ss,token,' ')){
-                if(!token.empty())
-                    tokens.push_back(token);                    
+        int len = s.length();
+        int count = 0;
+        int pos = 0;
+        for(int i = 0; i < len; i++){
+            if(isalnum(s[i]) && (s[i+1] == ' ' || s[i+1] == '\0')){
+                count++;
+                if(count == k){
+                    pos = i+1;
+                    break;
+                }
             }
-            return tokens;
-        };
-        string ans;
-        for(auto t : tokens(s)){
-            if(k == 0) break;
-            ans += t + " ";
-            k--;
         }
-        ans.pop_back();
-        return ans;
+        return s.substr(0, pos);
     }
 };
