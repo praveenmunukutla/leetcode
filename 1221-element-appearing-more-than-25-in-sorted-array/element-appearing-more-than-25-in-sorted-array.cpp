4d69f3f30b89da012233541831e7a944521f44a2
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 09:16:19 +0530

    Time: 13 ms (42.76%) | Memory: 15.1 MB (61.56%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
-        double special = 0.25 * arr.size();
+        double special = 0.25 * arr.size(); // 25% threshold for array size
         for(int i = 0; i < arr.size(); i++){
-            int val = arr[i];
+            int val = arr[i]; // Current value to evaluate
             int j = i+1;
-            while(j < arr.size() && arr[j] == val)j++;
-            double count = j - i;
-            //cout<<count<<" "<<val<<" "<<special<<endl;
-            if(count > special) return val;
+            while(j < arr.size() && arr[j] == val) j++; // Count consecutive occurrences
+            double count = j - i; // Calculate the count of `val`
+            i = j-1; // Skip over checked elements
+            if(count > special) return val; // Return `val` if it exceeds the threshold
         }
-
-        return 0;
+        return 0; // Default return, should never reach due to problem constraints
     }
 };
