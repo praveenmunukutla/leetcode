1fde740a8c7ec27727c6b47c25d5e5df1fc2c278
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 09:10:12 +0530

    Time: 4 ms (66.94%) | Memory: 12.3 MB (78.54%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
+    // int findNumbers(vector<int>& nums) {
+    //     int count = 0;
+    //     int tens[]={100000,10000,1000,100,10};
+    //     for(auto n : nums){
+    //         for(int i = 0, j = 5; i < 5; i++,j--){
+    //             if(n/tens[i] != 0){
+    //                 //contains j+1 digits;
+    //                 if((j+1)%2 == 0)
+    //                     count++;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
     int findNumbers(vector<int>& nums) {
         int count = 0;
-        int tens[]={100000,10000,1000,100,10};
         for(auto n : nums){
-            for(int i = 0, j = 5; i < 5; i++,j--){
-                if(n/tens[i] != 0){
-                    // cout<<n<<" "<<tens[i]<<" "<<j+1<<endl;
-                    //contains j digits;
-                    if((j+1)%2 == 0)
-                        count++;
-                    break;
-                }
+            int d = 0;
+            while(n){
+                d++;
+                n=n/10;
             }
+            if(d%2 == 0) count++;
         }
         return count;
     }
