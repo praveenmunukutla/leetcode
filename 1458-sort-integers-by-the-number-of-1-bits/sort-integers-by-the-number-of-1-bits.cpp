0e52c2ce8ea935044fe425e756da6911ca044b09
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 18:51:17 +0530

    Time: 4 ms (81.24%) | Memory: 12.7 MB (89.39%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -2,28 +2,18 @@ class Solution {
     unordered_map<int,int> umap;
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int,int>> ab;
-        for(auto a : arr){
-            ab.push_back({a, numberOfBits(a)});
-        }
+        sort(arr.begin(), arr.end(), [&](const auto& a, const auto& b){
 
-        sort(ab.begin(), ab.end(), [&](const auto& a, const auto& b){
-            if(a.second == b.second){
-                return a.first < b.first;
+            auto ac = __builtin_popcount(a);
+            auto bc = __builtin_popcount(b);
+
+            if(ac == bc){
+                return a < b;
             }
-            return a.second < b.second;
+
+            return ac < bc;
         });
 
-        vector<int> res;
-        for(auto a : ab) res.push_back(a.first);
-        return res;
-    }
-
-    int numberOfBits(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        
-        if(n%2 == 0) return numberOfBits(n/2);
-        return 1+numberOfBits(n/2);
+        return arr;
     }
 };
