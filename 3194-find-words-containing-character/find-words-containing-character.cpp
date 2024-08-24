25cfa87210cb13d15d0f845eadd714460bca9c33
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 19:00:24 +0530

    Time: 21 ms (33.64%) | Memory: 34 MB (10.38%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
-        vector<int> ans;
-        int index = 0;
-        for(auto w : words){
-            for(auto c : w){
-                if(c == x) {
-                    ans.push_back(index);
-                    break;
+        vector<int> ans; // Vector to store indices of words containing the character 'x'
+        int index = 0; // Initialize index to track the position of each word
+        
+        for(auto w : words) { // Iterate over each word in the list
+            for(auto c : w) { // Check each character in the current word
+                if(c == x) { // If the character matches 'x'
+                    ans.push_back(index); // Add the index of the word to the result
+                    break; // Stop checking further characters in this word
                 }
             }
-            index++;
+            index++; // Move to the next word
         }
-        return ans;
+        
+        return ans; // Return the list of indices
     }
 };
