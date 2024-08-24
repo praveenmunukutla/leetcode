fe5e3f58934f14013ad27cba0f1033e9e2633bf6
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 17:43:05 +0530

    Time: 10 ms (8.29%) | Memory: 13.4 MB (28.89%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         unordered_map<int,int> umap;
-        for(int i = 0; i < arr.size(); i++){
-            umap[arr[i]] = i;
-        }
 
         for(int i = 0; i < arr.size(); i++){
-            int val = arr[i]*2;
-            if(umap.find(val) != umap.end()){
-                if(i != umap[val])
-                    return true;
-            }
+            int dval = arr[i]*2;
+            int hval = arr[i]%2==0 ? arr[i]/2 : -1;
+            cout<<dval<<" "<<hval<<endl;
+
+            auto findInMap = [&](int val){
+                if(umap.find(val) != umap.end()) return true;
+                return false;
+            };
+
+            if(hval != -1 && findInMap(hval) && umap[hval] != i) return true;
+            else if(findInMap(dval) && umap[dval] != i) return true;
+
+            umap[arr[i]] = i;
         }
 
         
