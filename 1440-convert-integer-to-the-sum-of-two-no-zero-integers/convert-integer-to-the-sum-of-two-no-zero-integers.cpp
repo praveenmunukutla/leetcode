b4c5c265be7490763556b13993a8b356e973bcb3
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 09:30:19 +0530

    Time: 4 ms (17.60%) | Memory: 7.8 MB (47.49%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     vector<int> getNoZeroIntegers(int n) {
         for(int i = 1; i <= n/2; i++){
-            // Skip if either `i` or `n-i` contains '0'
-            if(cZero(i) || cZero(n-i)) continue;
-
-            // Return the pair of integers with no '0' digits
-            return {i, n-i};
+            // return `i` and `n-i`
+            if(!cZero(i) && !cZero(n-i)) return {i, n-i};
         }
 
         return {0,0}; // Default return, in case no valid pair is found
