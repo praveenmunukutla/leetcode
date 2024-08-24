917bf45000f4ebb3d57407a0500ed0e17fbece23
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 18:52:11 +0530

    Time: 6 ms (67.47%) | Memory: 13 MB (50.41%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,19 +1,18 @@
 class Solution {
-    unordered_map<int,int> umap;
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), [&](const auto& a, const auto& b){
+        // Sort array using a custom comparator based on the number of 1-bits in binary representation
+        sort(arr.begin(), arr.end(), [&](const auto& a, const auto& b) {
+            auto ac = __builtin_popcount(a); // Count the number of 1-bits in 'a'
+            auto bc = __builtin_popcount(b); // Count the number of 1-bits in 'b'
 
-            auto ac = __builtin_popcount(a);
-            auto bc = __builtin_popcount(b);
-
-            if(ac == bc){
-                return a < b;
+            if(ac == bc) {
+                return a < b; // If both have the same number of 1-bits, sort by the value itself
             }
 
-            return ac < bc;
+            return ac < bc; // Otherwise, sort by the number of 1-bits
         });
 
-        return arr;
+        return arr; // Return the sorted array
     }
 };
