0a15aa4978a70bb6cf88a3da1ae0fe5f0fa8a7b1
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-24 09:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (25.98%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     vector<int> getNoZeroIntegers(int n) {
         for(int i = 1; i <= n; i++){
+            // Lambda function to check if a number contains the digit '0'
             auto cZero = [](int val){
                 while(val){
-                    int d = val%10;
-                    if(d == 0)break;
-                    val /= 10;
+                    int d = val % 10;
+                    if(d == 0) break; // Break if any digit is '0'
+                    val /= 10; // Move to the next digit
                 }
-                return val ? true : false;
+                return val ? true : false; // Return true if '0' was found
             };
 
+            // Skip if either `i` or `n-i` contains '0'
             if(cZero(i) || cZero(n-i)) continue;
 
+            // Return the pair of integers with no '0' digits
             return {i, n-i};
         }
 
-        return {0,0};
+        return {0,0}; // Default return, in case no valid pair is found
     }
 };
