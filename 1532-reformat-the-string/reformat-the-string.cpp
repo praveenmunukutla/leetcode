85dc6b667237ffb719c9463960ec5d2a251e4ca6
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-08-30 08:01:40 +0530

    Time: 4 ms (80.65%) | Memory: 9.9 MB (11.72%) - LeetSync

---
+++ 1532-reformat-the-string/reformat-the-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reformat(string s) {
+        string dig = "";
+        string alpha = "";
+        for(auto c : s){
+            if(isdigit(c)) dig += c;
+            else alpha += c;
+        }
+
+        int dlen = dig.length();
+        int alen = alpha.length();
+        int diff = abs(dlen-alen);
+
+        if(diff > 1) return "";
+
+        if(diff == 0){
+            string ans = "";
+            for(int i = 0; i < dlen; i++){
+                ans += string(1,dig[i]) + string(1,alpha[i]);
+            }
+            return ans;
+        }
+
+        string gstr = dlen > alen ? dig : alpha;
+        string sstr = dlen > alen ? alpha : dig;
+        int slen = dlen > alen ? alen : dlen;
+
+        string ans = "";   
+        for(int i = 0; i < slen; i++){
+            ans += string(1,gstr[i]) + string(1,sstr[i]);
+        }
+
+        ans += string(1, gstr[gstr.length()-1]);
+        return ans;
+    }
+};
