5d075cf026b0c502ed5fd7ab12b420d1be924c61
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2024-09-04 13:31:24 +0530

    Time: 17 ms (57.79%) | Memory: 22.5 MB (33.63%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,30 +1,37 @@
 class MinStack {
-    stack<int> sta;
-    stack<int> msta;
+    stack<int> sta;  // Main stack to store all the elements
+    stack<int> msta; // Auxiliary stack to store the minimum values
+
 public:
     MinStack() {
-        
+        // Constructor: No initialization required as the stacks are automatically initialized
     }
     
+    // Push a new element onto the stack
     void push(int val) {
-        sta.push(val);
+        sta.push(val);  // Push the element onto the main stack
 
+        // If the min stack is empty, push the current element as the minimum
+        // Otherwise, push the minimum of the current element and the top of the min stack
         if(msta.empty()){
             msta.push(val);
-        }else{
-            val < msta.top() ? msta.push(val) : msta.push(msta.top());
+        } else {
+            msta.push(min(val, msta.top()));
         }
     }
     
+    // Pop the top element from both the main stack and the min stack
     void pop() {
         sta.pop();
         msta.pop();
     }
     
+    // Get the top element of the main stack
     int top() {
         return sta.top();
     }
     
+    // Get the minimum element from the min stack
     int getMin() {
         return msta.top();
     }
