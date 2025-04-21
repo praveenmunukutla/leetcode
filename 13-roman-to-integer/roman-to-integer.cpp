45ceea706c20ed89edb15576b3ec162501b21c7a
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2025-04-21 15:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (84.72%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,17 +1,48 @@
 class Solution {
-    unordered_map<char,int> map = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
 public:
     int romanToInt(string s) {
-        // roman to Int follow rules.
-        // only certain subtractive combinations are allowed.
-        int sum = 0, len = s.length();
-        for(int i = 0; i < len; i++){
-            if(i < len-1 && map[s[i]] < map[s[i+1]]){
-                sum -= map[s[i]];
-            }else{
-                sum += map[s[i]];
+        int value = 0;
+
+        for(int i = 0; s[i] != '\0'; i++){
+            if(s[i] == 'I' && s[i+1] != '\0' && s[i+1] == 'V'){
+                value += 4;
+                i++;            
+            }else if(s[i] == 'I' && s[i+1] != '\0' && s[i+1] == 'X'){
+                value += 9;                
+                i++;
+            }else if(s[i] == 'X' && s[i+1] != '\0' && s[i+1] == 'L'){
+                value += 40;                
+                i++;
+            }else if(s[i] == 'X' && s[i+1] != '\0' && s[i+1] == 'C'){
+                value += 90;                
+                i++;
+            }else if(s[i] == 'C' && s[i+1] != '\0' && s[i+1] == 'D'){
+                value += 400;                
+                i++;
+            }else if(s[i] == 'C' && s[i+1] != '\0' && s[i+1] == 'M'){
+                value += 900;                
+                i++;
+            }else if(s[i] == 'I'){
+                value += 1;
+            }else if(s[i] == 'V'){
+                value += 5;
+            }else if(s[i] == 'X'){
+                value += 10;
+            }else if(s[i] == 'L'){
+                value += 50;                
+            }else if(s[i] == 'C'){
+                value += 100;                
+            }else if(s[i] == 'D'){
+                value += 500;                
+            }else if(s[i] == 'M'){
+                value += 1000;                
             }
         }
-        return sum;
+
+        return value;
     }
 };
+
+// I V X
+// X L C
+// C D M
